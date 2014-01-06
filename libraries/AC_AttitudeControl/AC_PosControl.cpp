bb6f84dcc38c8aceef39f38450d8cbe3348bec66
Randy Mackay <rmackay9@yahoo.com>
2014-01-06 12:30:51 +0900

    AC_PosControl: minor format change

--- libraries/AC_AttitudeControl/AC_PosControl.cpp
+++ libraries/AC_AttitudeControl/AC_PosControl.cpp
@@ -265,7 +265,7 @@ void AC_PosControl::accel_to_throttle(float accel_target_z)
     // To-Do: where to get hover throttle?
     // To-Do: we had a contraint here but it's now removed, is this ok?  with the motors library handle it ok?
     _attitude_control.set_throttle_out((int16_t)p+i+d+_throttle_hover, true);
-
+    
     // to-do add back in PID logging?
 }
 /*
