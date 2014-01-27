822a3cee2a4bebf794929877b79a43f74331b2ea
Randy Mackay <rmackay9@yahoo.com>
2014-01-27 23:42:14 +0900

    AC_Circle: mostly working circle controller

--- libraries/AC_WPNav/AC_Circle.cpp
+++ libraries/AC_WPNav/AC_Circle.cpp
@@ -35,9 +35,9 @@ AC_Circle::AC_Circle(const AP_InertialNav& inav, const AP_AHRS& ahrs, AC_PosCont
     _ahrs(ahrs),
     _pos_control(pos_control),
     _last_update(0),
-    _step(0),
-    _yaw(0),
-    _angle(0)
+    _angle(0),
+    _cos_yaw(1.0),
+    _sin_yaw(0.0)
 {
     AP_Param::setup_object_defaults(this, var_info);
 }
@@ -47,13 +47,13 @@ void AC_Circle::set_center(const Vector3f& position)
 {
     _center = position;
 
-	// set target position
+	// To-Do: set target position, angle, etc so that copter begins circle from closest point to stopping point
 	_pos_control.set_pos_target(_inav.get_position());
 
-	// initialise pos controller speed and acceleration
-	// To-Do: pull these from waypoint controller?
-	_pos_control.set_speed_xy(500);
-    _pos_control.set_accel_xy(100);
+	// To-Do: set _pos_control speed and accel
+
+    // calculate velocities
+    calc_velocities();
 }
 
 /// init_center in cm from home using stopping point and projecting out based on the copter's heading
@@ -65,46 +65,16 @@ void AC_Circle::init_center()
     _pos_control.get_stopping_point_xy(stopping_point);
     _pos_control.get_stopping_point_z(stopping_point);
 
-    // To-Do: replace this simple circle center calculation with projected position
-    _center = stopping_point;
-/*
     // set circle center to circle_radius ahead of stopping point
     _center.x = stopping_point.x + _radius * _cos_yaw;
     _center.y = stopping_point.y + _radius * _sin_yaw;
     _center.z = stopping_point.z;
 
-    // if we are doing a panorama set the circle_angle to the current heading
-    if (_radius <= 0) {
-        _angle = heading_in_radians;
-        _angular_velocity_max = ToRad(g.circle_rate);
-        _angular_acceleration = _angular_velocity_max;  // reach maximum yaw velocity in 1 second
-    }else{
-        // set starting angle to current heading - 180 degrees
-        circle_angle = wrap_PI(heading_in_radians-PI);
+    // update pos_control target to stopping point
+    _pos_control.set_pos_target(stopping_point);
 
-        // calculate max velocity based on waypoint speed ensuring we do not use more than half our max acceleration for accelerating towards the center of the circle
-        max_velocity = min(wp_nav.get_horizontal_velocity(), safe_sqrt(0.5f*wp_nav.get_wp_acceleration()*g.circle_radius*100.0f));
-
-        // angular_velocity in radians per second
-        circle_angular_velocity_max = max_velocity/((float)g.circle_radius * 100.0f);
-        circle_angular_velocity_max = constrain_float(ToRad(g.circle_rate),-circle_angular_velocity_max,circle_angular_velocity_max);
-
-        // angular_velocity in radians per second
-        circle_angular_acceleration = wp_nav.get_wp_acceleration()/((float)g.circle_radius * 100);
-        if (g.circle_rate < 0.0f) {
-            circle_angular_acceleration = -circle_angular_acceleration;
-        }
-    }
-
-        // initialise other variables
-        circle_angle_total = 0;
-        circle_angular_velocity = 0;
-
-        // initialise loiter target.  Note: feed forward velocity set to zero
-        // To-Do: modify circle to use position controller and pass in zero velocity.  Vector3f(0,0,0)
-        wp_nav.init_loiter_target();
-    }
-*/
+    // calculate velocities
+    calc_velocities();
 }
 
 /// update - update circle controller
@@ -114,21 +84,96 @@ void AC_Circle::update()
     uint32_t now = hal.scheduler->millis();
     float dt = (now - _last_update) / 1000.0f;
 
-    // reset step back to 0 if 0.1 seconds has passed and we completed the last full cycle
+    // update circle position at 10hz
     if (dt > 0.095f) {
+
         // double check dt is reasonable
         if (dt >= 1.0f) {
             dt = 0.0;
         }
-
-        // To-Do: update the pos controller target
-
         // capture time since last iteration
         _last_update = now;
+
+        // ramp up angular velocity to maximum
+        if (_rate >= 0) {
+            if (_angular_vel < _angular_vel_max) {
+                _angular_vel += _angular_accel * dt;
+                _angular_vel = constrain_float(_angular_vel, 0, _angular_vel_max);
+            }
+        }else{
+            if (_angular_vel > _angular_vel_max) {
+                _angular_vel += _angular_accel * dt;
+                _angular_vel = constrain_float(_angular_vel, _angular_vel_max, 0);
+            }
+        }
+
+        // update the target angle and total angle traveled
+        float angle_change = _angular_vel * dt;
+        _angle += angle_change;
+        _angle = wrap_PI(_angle);
+        _angle_total += angle_change;
+
+        // heading is 180 deg from vehicles target position around circle
+        _yaw = wrap_PI(_angle-PI);
+
+        // if the circle_radius is zero we are doing panorama so no need to update loiter target
+        if (_radius != 0.0) {
+            // calculate target position
+            Vector3f target;
+            target.x = _center.x + _radius * cosf(-_angle);
+            target.y = _center.y - _radius * sinf(-_angle);
+            target.z = _pos_control.get_alt_target();
+
+            // update position controller target
+            _pos_control.set_pos_target(target);
+
+            // heading is 180 deg from vehicles target position around circle
+            _yaw = wrap_PI(_angle-PI);
+        }else{
+            // heading is 180 deg from vehicles target position around circle
+            _yaw = wrap_PI(_angle-PI);
+        }
+
         // trigger position controller on next update
         _pos_control.trigger_xy();
-    }else{
-        // run loiter's position to velocity step
-        _pos_control.update_pos_controller(true);
     }
+
+    // run loiter's position to velocity step
+    _pos_control.update_pos_controller(false);
+}
+
+// calc_velocities - calculate angular velocity max and acceleration based on radius and rate
+//      this should be called whenever the radius or rate are changed
+//      initialises the yaw and current position around the circle
+void AC_Circle::calc_velocities()
+{
+    // if we are doing a panorama set the circle_angle to the current heading
+    if (_radius <= 0) {
+        _angle = _ahrs.yaw;
+        _angular_vel_max = ToRad(_rate);
+        _angular_accel = _angular_vel_max;  // reach maximum yaw velocity in 1 second
+    }else{
+        // set starting angle to current heading - 180 degrees
+        _angle = wrap_PI(_ahrs.yaw-PI);
+
+        // calculate max velocity based on waypoint speed ensuring we do not use more than half our max acceleration for accelerating towards the center of the circle
+        float velocity_max = min(_pos_control.get_speed_xy(), safe_sqrt(0.5f*_pos_control.get_accel_xy()*_radius));
+
+        // angular_velocity in radians per second
+        _angular_vel_max = velocity_max/_radius;
+        _angular_vel_max = constrain_float(ToRad(_rate),-_angular_vel_max,_angular_vel_max);
+
+        // angular_velocity in radians per second
+        _angular_accel = _pos_control.get_accel_xy()/_radius;
+        if (_rate < 0.0f) {
+            _angular_accel = -_angular_accel;
+        }
+    }
+
+    // debug -- remove me!
+    //hal.console->printf_P(PSTR("\nPan Ang:%4.2f AVM:%4.2f Acc:%4.2f\n"),(float)_angle,(float)_angular_vel_max,(float)_angular_accel);
+
+    // initialise other variables
+    _angle_total = 0;
+    _angular_vel = 0;
 }
--- libraries/AC_WPNav/AC_Circle.h
+++ libraries/AC_WPNav/AC_Circle.h
@@ -28,13 +28,16 @@ public:
     void init_center();
 
     /// get_circle_center in cm from home
-    const Vector3f& get_center() { return _center; }
+    const Vector3f& get_center() const { return _center; }
 
     /// set_radius - sets circle radius in cm
     void set_radius(float radius_cm) { _radius = radius_cm; };
 
     /// set_circle_rate - set circle rate in degrees per second
     void set_rate(float deg_per_sec) { _rate = deg_per_sec; }
+
+    /// get_angle_total - return total angle in radians that vehicle has circled
+    float get_angle_total() const { return _angle_total; }
 
     /// update - update circle controller
     void update();
@@ -44,9 +47,21 @@ public:
     int32_t get_pitch() const { return _pos_control.get_pitch(); };
     int32_t get_yaw() const { return _yaw; };
 
+    /// set_cos_sin_yaw - short-cut to save on calculations to convert from roll-pitch frame to lat-lon frame
+    void set_cos_sin_yaw(float cos_yaw, float sin_yaw) {
+        _cos_yaw = cos_yaw;
+        _sin_yaw = sin_yaw;
+    }
+
     static const struct AP_Param::GroupInfo var_info[];
 
 private:
+
+    // calc_velocities - calculate angular velocity max and acceleration based on radius and rate
+    //      this should be called whenever the radius or rate are changed
+    //      initialises the yaw and current position around the circle
+    void calc_velocities();
+
     // flags structure
     struct circle_flags {
         uint8_t panorama    : 1;    // true if we are doing a panorama
@@ -64,9 +79,16 @@ private:
 
     // internal variables
     uint32_t    _last_update;   // time of last update_loiter call
-    uint8_t     _step;          // used to decide which portion of loiter controller to run during this iteration
     Vector3f    _center;        // center of circle in cm from home
     float       _yaw;           // yaw heading (normally towards circle center)
-    float       _angle;         // current angular position around circle (0=directly north of the center of teh circle)
+    float       _angle;         // current angular position around circle in radians (0=directly north of the center of the circle)
+    float       _angle_total;   // total angle travelled in radians
+    float       _angular_vel;   // angular velocity in radians/sec
+    float       _angular_vel_max;   // maximum velocity in radians/sec
+    float       _angular_accel; // angular acceleration in radians/sec/sec
+
+    // helper variables
+    float       _cos_yaw;       // short-cut to save on calcs required to convert roll-pitch frame to lat-lon frame
+    float       _sin_yaw;       // To-Do: move these to ahrs or attitude control class to save on memory
 };
 #endif	// AC_CIRCLE_H
