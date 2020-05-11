c176c53ebb4cd54058a52214e1ea648070756236
Randy Mackay <rmackay9@yahoo.com>
2020-05-11 14:15:15 +0900

    Copter: version to 4.0.3-harris
    This version is ArduPilot Copter-4.0.3 with two additions from Copter-4.0.4 which are landing detector filter improvements and disabling of DCM yaw prearm check

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.0.3"
+#define THISFIRMWARE "ArduCopter V4.0.3-harris"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,3,FIRMWARE_VERSION_TYPE_OFFICIAL
