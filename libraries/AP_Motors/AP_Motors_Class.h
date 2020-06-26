9404f1025f91fc85961e421d9e135f4e6a0ccab2
Randy Mackay <rmackay9@yahoo.com>
2020-06-26 18:31:37 +0900

    AP_Motors: minor format fix

--- libraries/AP_Motors/AP_Motors_Class.h
+++ libraries/AP_Motors/AP_Motors_Class.h
@@ -127,9 +127,9 @@ public:
     enum class SpoolState : uint8_t {
         SHUT_DOWN = 0,                      // all motors stop
         GROUND_IDLE = 1,                    // all motors at ground idle
-        SPOOLING_UP = 2,                       // increasing maximum throttle while stabilizing
+        SPOOLING_UP = 2,                    // increasing maximum throttle while stabilizing
         THROTTLE_UNLIMITED = 3,             // throttle is no longer constrained by start up procedure
-        SPOOLING_DOWN = 4,                     // decreasing maximum throttle while stabilizing
+        SPOOLING_DOWN = 4,                  // decreasing maximum throttle while stabilizing
     };
 
     // get_spool_state - get current spool state
