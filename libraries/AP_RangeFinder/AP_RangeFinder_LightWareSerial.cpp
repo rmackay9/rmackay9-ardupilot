8b543beeea19f54b6e27f75ca93a3f6be26a769c
Randy Mackay <rmackay9@yahoo.com>
2020-07-07 13:10:37 +0900

    AP_RangeFinder: add Lightware Serial advanced protocol support

--- libraries/AP_RangeFinder/AP_RangeFinder_LightWareSerial.cpp
+++ libraries/AP_RangeFinder/AP_RangeFinder_LightWareSerial.cpp
@@ -11,17 +11,27 @@
 
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   Advanced serial protocol is described here:
+       Packet structure: https://github.com/LightWare-Optoelectronics/LW20-docs/blob/master/serial.md
+       Commands: https://github.com/LightWare-Optoelectronics/LW20-docs/blob/master/command_detail.md
  */
 
 #include "AP_RangeFinder_LightWareSerial.h"
 
 #include <AP_HAL/AP_HAL.h>
+#include <AP_HAL/utility/sparse-endian.h>
+#include <AP_Math/crc.h>
 #include <ctype.h>
+#include <AP_Logger/AP_Logger.h>
 
 extern const AP_HAL::HAL& hal;
 
-#define LIGHTWARE_DIST_MAX_CM           10000
-#define LIGHTWARE_OUT_OF_RANGE_ADD_CM   100
+#define LIGHTWARE_DIST_MAX_CM                       10000
+#define LIGHTWARE_OUT_OF_RANGE_ADD_CM               100
+#define LIGHTWARE_ADVANCED_HEADER                   0xAA
+#define LIGHTWARE_ADVANCED_STREAMING_DISTANCE_DATA  5
+#define LIGHTWARE_ADVANCED_DISTANCE_OUTPUT_CONFIG_BITMASK   0xFF    // bitmask of desired distance data (lowest 8 bits set)
 
 // read - return last value measured by sensor
 bool AP_RangeFinder_LightWareSerial::get_reading(uint16_t &reading_cm)
@@ -89,6 +99,19 @@ bool AP_RangeFinder_LightWareSerial::get_reading(uint16_t &reading_cm)
                 high_byte_received = false;
             }
         }
+
+        // use advanced serial protocol
+        if (protocol_state == ProtocolState::ADVANCED) {
+            if (advanced_parse_byte(c)) {
+                int16_t dist_cm;
+                if (advanced_process_message(dist_cm)) {
+                    if (dist_cm >= 0) {
+                        sum += dist_cm * 0.01f;
+                        valid_count++;
+                    }
+                }
+            }
+        }
     }
 
     // protocol set after 10 successful reads
@@ -113,6 +136,15 @@ bool AP_RangeFinder_LightWareSerial::get_reading(uint16_t &reading_cm)
         uart->write('d');
     }
 
+    // if using advanced serial protocol, send request to start streaming
+    // if distances data messages are not arriving
+    if (protocol_state == ProtocolState::ADVANCED &&
+        (now - _advance_last_distance_ms > 1000) &&
+        (now - _advance_last_req_ms > 1000)) {
+        advanced_request_stream_start();
+        _advance_last_req_ms = now;
+    }
+
     // return average of all valid readings
     if (valid_count > 0) {
         reading_cm = 100 * sum / valid_count;
@@ -127,4 +159,268 @@ bool AP_RangeFinder_LightWareSerial::get_reading(uint16_t &reading_cm)
 
     // no readings so return false
     return false;
+}
+
+// send message to lidar using advanced serial protocol
+void AP_RangeFinder_LightWareSerial::advanced_send_message(AdvancedMessageID msgid, bool write, const uint8_t *payload, uint16_t payload_len)
+{
+    if ((uart == nullptr) || (payload_len > LIGHTWARE_ADVANCED_PAYLOAD_LEN_MAX)) {
+        return;
+    }
+
+    // check for sufficient space in outgoing buffer
+    if (uart->txspace() < payload_len + 6U) {
+        return;
+    }
+
+    // write header
+    uart->write((uint8_t)LIGHTWARE_ADVANCED_HEADER);
+    uint16_t crc = crc_xmodem_update(0, LIGHTWARE_ADVANCED_HEADER);
+
+    // write flags including payload length
+    const uint16_t flags = ((payload_len+1) << 6) | (write ? 0x01 : 0);
+    uart->write(LOWBYTE(flags));
+    crc = crc_xmodem_update(crc, LOWBYTE(flags));
+    uart->write(HIGHBYTE(flags));
+    crc = crc_xmodem_update(crc, HIGHBYTE(flags));
+
+    // msgid
+    uart->write((uint8_t)msgid);
+    crc = crc_xmodem_update(crc, (uint8_t)msgid);
+
+    // payload
+    if ((payload_len > 0) && (payload != nullptr)) {
+        for (uint16_t i = 0; i < payload_len; i++) {
+            uart->write(payload[i]);
+            crc = crc_xmodem_update(crc, payload[i]);
+        }
+    }
+
+    // checksum
+    uart->write(LOWBYTE(crc));
+    uart->write(HIGHBYTE(crc));
+}
+
+// request sending of distance data using advanced serial protocol
+void AP_RangeFinder_LightWareSerial::advanced_request_stream_start()
+{
+    // request product name to switch sensor to advanced serial protocol
+    advanced_send_message(AdvancedMessageID::PRODUCT_NAME, false, nullptr, 0);
+
+    // request which data we would like in distance data messages
+    uint8_t desired_data_buff[] = {LIGHTWARE_ADVANCED_DISTANCE_OUTPUT_CONFIG_BITMASK, 0x0, 0x0, 0x0};
+    advanced_send_message(AdvancedMessageID::DISTANCE_OUTPUT_CONFIG, true, desired_data_buff, ARRAY_SIZE(desired_data_buff));
+
+    // request streaming to start
+    const le32_t stream_req = htole32(LIGHTWARE_ADVANCED_STREAMING_DISTANCE_DATA);
+    advanced_send_message(AdvancedMessageID::STREAM, true, (const uint8_t*)&stream_req, sizeof(stream_req));
+}
+
+// process one byte received on serial port
+// returns true if a complete message has been received, message is stored in _advanced_msg structure
+bool AP_RangeFinder_LightWareSerial::advanced_parse_byte(uint8_t b)
+{
+    // check that payload buffer is large enough
+    static_assert(ARRAY_SIZE(_advanced_msg.payload) == LIGHTWARE_ADVANCED_PAYLOAD_LEN_MAX, "AP_Proximity_LightwareSF40C: check _advanced_msg.payload array size ");
+
+    // process byte depending upon current state
+    switch (_advanced_parse_state) {
+
+    case AdvancedParseState::HEADER:
+        if (b == LIGHTWARE_ADVANCED_HEADER) {
+            _advanced_msg.crc_expected = crc_xmodem_update(0, b);
+            _advanced_parse_state = AdvancedParseState::FLAGS_L;
+        }
+        break;
+
+    case AdvancedParseState::FLAGS_L:
+        _advanced_msg.flags_low = b;
+        _advanced_msg.crc_expected = crc_xmodem_update(_advanced_msg.crc_expected, b);
+        _advanced_parse_state = AdvancedParseState::FLAGS_H;
+        break;
+
+    case AdvancedParseState::FLAGS_H:
+        _advanced_msg.flags_high = b;
+        _advanced_msg.crc_expected = crc_xmodem_update(_advanced_msg.crc_expected, b);
+        _advanced_msg.payload_len = (UINT16_VALUE(_advanced_msg.flags_high, _advanced_msg.flags_low) >> 6) & 0x3FF;
+        if ((_advanced_msg.payload_len == 0) || (_advanced_msg.payload_len > LIGHTWARE_ADVANCED_PAYLOAD_LEN_MAX)) {
+            // invalid payload length, abandon message
+            _advanced_parse_state = AdvancedParseState::HEADER;
+        } else {
+            _advanced_parse_state = AdvancedParseState::MSG_ID;
+        }
+        break;
+
+    case AdvancedParseState::MSG_ID:
+        _advanced_msg.msgid = (AdvancedMessageID)b;
+        _advanced_msg.crc_expected = crc_xmodem_update(_advanced_msg.crc_expected, b);
+        if (_advanced_msg.payload_len > 1) {
+            _advanced_parse_state = AdvancedParseState::PAYLOAD;
+        } else {
+            _advanced_parse_state = AdvancedParseState::CRC_L;
+        }
+        _advanced_msg.payload_recv = 0;
+        break;
+
+    case AdvancedParseState::PAYLOAD:
+        if (_advanced_msg.payload_recv < (_advanced_msg.payload_len - 1)) {
+            _advanced_msg.payload[_advanced_msg.payload_recv] = b;
+            _advanced_msg.payload_recv++;
+            _advanced_msg.crc_expected = crc_xmodem_update(_advanced_msg.crc_expected, b);
+        }
+        if (_advanced_msg.payload_recv >= (_advanced_msg.payload_len - 1)) {
+            _advanced_parse_state = AdvancedParseState::CRC_L;
+        }
+        break;
+
+    case AdvancedParseState::CRC_L:
+        _advanced_msg.crc_low = b;
+        _advanced_parse_state = AdvancedParseState::CRC_H;
+        break;
+
+    case AdvancedParseState::CRC_H:
+        _advanced_parse_state = AdvancedParseState::HEADER;
+        _advanced_msg.crc_high = b;
+        const bool crc_ok = _advanced_msg.crc_expected == UINT16_VALUE(_advanced_msg.crc_high, _advanced_msg.crc_low);
+        return crc_ok;
+    }
+
+    return false;
+}
+
+// process the latest message held in the _advanced_msg structure
+// returns true if a distance was received and places distance in distance_cm argument
+bool AP_RangeFinder_LightWareSerial::advanced_process_message(int16_t &distance_cm)
+{
+    // process payload
+    switch (_advanced_msg.msgid) {
+
+    case AdvancedMessageID::DISTANCE_OUTPUT:
+    case AdvancedMessageID::DISTANCE_OUTPUT_CONFIG:
+    {
+        // consume only the lowest byte
+        _advanced_sensor_state.distance_output_config = _advanced_msg.payload[0];
+        break;
+    }
+
+    case AdvancedMessageID::STREAM:
+        if (_advanced_msg.payload_recv == sizeof(uint32_t)) {
+            // valid stream values are 0:disabled, 1:raw distance data, 5:distance data, 10:signal probability data
+            const uint32_t stream_value = buff_to_uint32(_advanced_msg.payload[0], _advanced_msg.payload[1], _advanced_msg.payload[2], _advanced_msg.payload[3]);
+            _advanced_sensor_state.streaming = (stream_value == LIGHTWARE_ADVANCED_STREAMING_DISTANCE_DATA);
+        }
+        break;
+
+    case AdvancedMessageID::DISTANCE_DATA: {
+        // we should have received eight 2byte values
+        if (_advanced_msg.payload_recv != 16) {
+            return false;
+        }
+
+        // places extra data into the correct place in _advanced_distance_output
+        _advanced_distance_output.first_dist_raw_cm = buff_to_int16(_advanced_msg.payload[0], _advanced_msg.payload[1]);
+        _advanced_distance_output.first_dist_filt_cm = buff_to_int16(_advanced_msg.payload[2], _advanced_msg.payload[3]);
+        _advanced_distance_output.first_strength_pct  = buff_to_int16(_advanced_msg.payload[4], _advanced_msg.payload[5]);
+        _advanced_distance_output.last_dist_raw_cm = buff_to_int16(_advanced_msg.payload[6], _advanced_msg.payload[7]);
+        _advanced_distance_output.last_dist_filt_cm = buff_to_int16(_advanced_msg.payload[8], _advanced_msg.payload[9]);
+        _advanced_distance_output.last_strength_pct = buff_to_int16(_advanced_msg.payload[10], _advanced_msg.payload[11]);
+        _advanced_distance_output.noise = buff_to_int16(_advanced_msg.payload[12], _advanced_msg.payload[13]);
+        _advanced_distance_output.temp_cd = buff_to_int16(_advanced_msg.payload[14], _advanced_msg.payload[15]);
+
+        // return first distance (filtered)
+        distance_cm = _advanced_distance_output.first_dist_filt_cm;
+        _advance_last_distance_ms = AP_HAL::millis();
+
+        // log extra data held in _advanced_distance_output
+        Log_RFNE();
+
+        // return true to indicate new distance has been consumed
+        return true;
+    }
+
+    // unsupported messages
+    case AdvancedMessageID::PRODUCT_NAME:
+    case AdvancedMessageID::HARDWARE_VERSION:
+    case AdvancedMessageID::FIRMWARE_VERSION:
+    case AdvancedMessageID::SERIAL_NUMBER:
+    case AdvancedMessageID::TEXT_MESSAGE:
+    case AdvancedMessageID::USER_DATA:
+    case AdvancedMessageID::TOKEN:
+    case AdvancedMessageID::SAVE_PARAMETERS:
+    case AdvancedMessageID::RESET:
+    case AdvancedMessageID::STAGE_FIRMWARE:
+    case AdvancedMessageID::COMMIT_FIRMWARE:
+    case AdvancedMessageID::INCOMING_VOLTAGE:
+    case AdvancedMessageID::COMMUNICATION_MODE:
+    case AdvancedMessageID::STREAM_STATISTICS:
+    case AdvancedMessageID::STATISTICS_DATA:
+    case AdvancedMessageID::RAW_DISTANCE_DATA:
+    case AdvancedMessageID::SIGNAL_PROBABILITY_DATA:
+    case AdvancedMessageID::LASER_FIRING:
+    case AdvancedMessageID::TEMPERATURE:
+    case AdvancedMessageID::HIGH_SPEED_MODE:
+    case AdvancedMessageID::NOISE:
+    case AdvancedMessageID::ALARM_STATUS:
+    case AdvancedMessageID::BAUD_RATE:
+    case AdvancedMessageID::I2C_ADDRESS:
+    case AdvancedMessageID::MEASUREMENT_MODE:
+    case AdvancedMessageID::ZERO_OFFSET:
+    case AdvancedMessageID::LOST_SIGNAL_COUNTER:
+    case AdvancedMessageID::ALARM_A_DISTANCE:
+    case AdvancedMessageID::ALARM_B_DISTANCE:
+    case AdvancedMessageID::ALARM_HYSTERESIS:
+    case AdvancedMessageID::SERVO_CONNECTED:
+    case AdvancedMessageID::SERVO_SCANNING:
+    case AdvancedMessageID::SERVO_POSITION:
+    case AdvancedMessageID::SERVO_PWM_LOW:
+    case AdvancedMessageID::SERVO_PWM_HIGH:
+    case AdvancedMessageID::SERVO_PWM_SCALE:
+    case AdvancedMessageID::SERVO_SCAN_TYPE:
+    case AdvancedMessageID::SERVO_SCAN_SPEED:
+    case AdvancedMessageID::SERVO_LAG:
+    case AdvancedMessageID::SERVO_FOV_LOW:
+    case AdvancedMessageID::SERVO_FOV_HIGH:
+    case AdvancedMessageID::SERVO_ALARM_A_LOW:
+    case AdvancedMessageID::SERVO_ALARM_A_HIGH:
+    case AdvancedMessageID::SERVO_ALARM_B_LOW:
+    case AdvancedMessageID::SERVO_ALARM_B_HIGH:
+        break;
+    }
+
+    return false;
+}
+
+// convert buffer to uint32, uint16
+uint32_t AP_RangeFinder_LightWareSerial::buff_to_uint32(uint8_t b0, uint8_t b1, uint8_t b2, uint8_t b3) const
+{
+    uint32_t leval = (uint32_t)b0 | (uint32_t)b1 << 8 | (uint32_t)b2 << 16 | (uint32_t)b3 << 24;
+    return leval;
+}
+
+int16_t AP_RangeFinder_LightWareSerial::buff_to_int16(uint8_t b0, uint8_t b1) const
+{
+    uint16_t leval = (uint16_t)b0 | (uint16_t)b1 << 8;
+    return (int16_t)leval;
+}
+
+// log extra distances, strengths, noise and temperature
+void AP_RangeFinder_LightWareSerial::Log_RFNE()
+{
+#ifndef HAL_BUILD_AP_PERIPH
+    const struct log_RFNE pkt = {
+            LOG_PACKET_HEADER_INIT(LOG_RFNE_MSG),
+            time_us             : AP_HAL::micros64(),
+            instance            : 0,    // we do not know our instance
+            first_dist_raw_cm   : _advanced_distance_output.first_dist_raw_cm,
+            first_dist_filt_cm  : _advanced_distance_output.first_dist_filt_cm,
+            first_strength_pct  : _advanced_distance_output.first_strength_pct,
+            last_dist_raw_cm    : _advanced_distance_output.last_dist_raw_cm,
+            last_dist_filt_cm   : _advanced_distance_output.last_dist_filt_cm,
+            last_strength_pct   : _advanced_distance_output.last_strength_pct,
+            noise               : _advanced_distance_output.noise,
+            temp_cd             : _advanced_distance_output.temp_cd
+    };
+
+    AP::logger().WriteBlock(&pkt, sizeof(pkt));
+#endif
 }
--- libraries/AP_RangeFinder/AP_RangeFinder_LightWareSerial.h
+++ libraries/AP_RangeFinder/AP_RangeFinder_LightWareSerial.h
@@ -3,6 +3,8 @@
 #include "AP_RangeFinder.h"
 #include "AP_RangeFinder_Backend_Serial.h"
 
+#define LIGHTWARE_ADVANCED_PAYLOAD_LEN_MAX  30      // packets are payload + 6 bytes (header, flags low+high, message id, payload, crc low+high)
+
 class AP_RangeFinder_LightWareSerial : public AP_RangeFinder_Backend_Serial
 {
 
@@ -20,6 +22,123 @@ private:
     // get a reading
     bool get_reading(uint16_t &reading_cm) override;
 
+    // advanced serial protocol message ids
+    enum class AdvancedMessageID : uint8_t {
+        PRODUCT_NAME = 0,
+        HARDWARE_VERSION = 1,
+        FIRMWARE_VERSION = 2,
+        SERIAL_NUMBER = 3,
+        TEXT_MESSAGE = 7,
+        USER_DATA = 9,
+        TOKEN = 10,
+        SAVE_PARAMETERS = 12,
+        RESET = 14,
+        STAGE_FIRMWARE = 16,
+        COMMIT_FIRMWARE = 17,
+        INCOMING_VOLTAGE = 20,
+        DISTANCE_OUTPUT = 27,
+        COMMUNICATION_MODE = 28,
+        DISTANCE_OUTPUT_CONFIG = 29,
+        STREAM = 30,
+        STREAM_STATISTICS = 31,
+        STATISTICS_DATA = 35,
+        RAW_DISTANCE_DATA = 40,
+        SIGNAL_PROBABILITY_DATA = 43,
+        DISTANCE_DATA = 44,
+        LASER_FIRING = 50,
+        TEMPERATURE = 55,
+        HIGH_SPEED_MODE = 70,
+        NOISE = 85,
+        ALARM_STATUS = 88,
+        BAUD_RATE = 90,
+        I2C_ADDRESS = 91,
+        MEASUREMENT_MODE = 93,
+        ZERO_OFFSET = 94,
+        LOST_SIGNAL_COUNTER = 95,
+        ALARM_A_DISTANCE = 96,
+        ALARM_B_DISTANCE = 97,
+        ALARM_HYSTERESIS = 98,
+        SERVO_CONNECTED = 121,
+        SERVO_SCANNING = 122,
+        SERVO_POSITION = 123,
+        SERVO_PWM_LOW = 124,
+        SERVO_PWM_HIGH = 125,
+        SERVO_PWM_SCALE = 126,
+        SERVO_SCAN_TYPE = 127,
+        SERVO_SCAN_SPEED = 128,
+        SERVO_LAG = 129,
+        SERVO_FOV_LOW = 130,
+        SERVO_FOV_HIGH = 131,
+        SERVO_ALARM_A_LOW = 132,
+        SERVO_ALARM_A_HIGH = 133,
+        SERVO_ALARM_B_LOW = 134,
+        SERVO_ALARM_B_HIGH = 135,
+    };
+
+    // send message to sensor using advanced serial protocol
+    void advanced_send_message(AdvancedMessageID msgid, bool write, const uint8_t *payload, uint16_t payload_len);
+
+    // request sending of distance data using advanced serial protocol
+    void advanced_request_stream_start();
+
+    // process one byte received on serial port using advanced serial protocol
+    // returns true if a complete message has been received, message is stored in _advanced_msg structure
+    bool advanced_parse_byte(uint8_t b);
+
+    // process the latest message held in the _advanced_msg structure
+    // returns true if a distance was received and places distance in distance_cm argument
+    bool advanced_process_message(int16_t &distance_cm);
+
+    // convert buffer to uint32, uint16
+    uint32_t buff_to_uint32(uint8_t b0, uint8_t b1, uint8_t b2, uint8_t b3) const;
+    int16_t buff_to_int16(uint8_t b0, uint8_t b1) const;
+
+    // log extra distances, strengths, noise and temperature
+    void Log_RFNE();
+
+    enum class AdvancedParseState : uint8_t {
+        HEADER = 0,
+        FLAGS_L,
+        FLAGS_H,
+        MSG_ID,
+        PAYLOAD,
+        CRC_L,
+        CRC_H
+    } _advanced_parse_state;
+    uint32_t _advance_last_req_ms;      // system time of last request for distance data to be streamed
+    uint32_t _advance_last_distance_ms; // system time of last distance data packet received
+
+    // sensor state retrieved using advanced serial protocol
+    struct {
+        bool streaming;                 // true if distance messages are being streamed
+        uint8_t distance_output_config; // bitmask of extra info being sent
+    } _advanced_sensor_state;
+
+    // extra data retrieved from sensor in distance_output messages
+    struct {
+        int16_t first_dist_raw_cm;
+        int16_t first_dist_filt_cm;
+        int16_t first_strength_pct;
+        int16_t last_dist_raw_cm;
+        int16_t last_dist_filt_cm;
+        int16_t last_strength_pct;
+        int16_t noise;
+        int16_t temp_cd;
+    } _advanced_distance_output;
+
+    // latest advanced message contents
+    struct {
+        uint8_t flags_low;      // flags low byte
+        uint8_t flags_high;     // flags high byte
+        uint16_t payload_len;   // latest message payload length including msgid byte (i.e. bytes in payload +1)
+        uint8_t payload[LIGHTWARE_ADVANCED_PAYLOAD_LEN_MAX];    // payload
+        AdvancedMessageID msgid;// latest message's message id
+        uint16_t payload_recv;  // number of message's payload bytes received so far (does not include msgid byte)
+        uint8_t crc_low;        // crc low byte
+        uint8_t crc_high;       // crc high byte
+        uint16_t crc_expected;  // latest message's expected crc
+    } _advanced_msg;
+
     char linebuf[10];           // legacy protocol buffer
     uint8_t linebuf_len;        // legacy protocol buffer length
     uint32_t last_init_ms;      // init time used to switch lw20 to serial mode
@@ -30,8 +149,9 @@ private:
     enum class ProtocolState {
         UNKNOWN,    // the protocol used is not yet known
         LEGACY,     // legacy protocol, distances are sent as strings
-        BINARY      // binary protocol, distances are sent using two bytes
-    } protocol_state;
+        BINARY,     // binary protocol, distances are sent using two bytes
+        ADVANCED    // advanced serial protocol, allows changing settings, packets include header, payload and checksum
+    } protocol_state = ProtocolState::ADVANCED;
     uint8_t legacy_valid_count;
     uint8_t binary_valid_count;
 };
