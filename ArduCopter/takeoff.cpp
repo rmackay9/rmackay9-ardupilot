4b6a954b191ac04ca8e3f3463d68bc18018e9d7c
Tatsuya Yamaguchi <susumu-336@hotmail.co.jp>
2020-11-11 09:22:11 +0900

    Copter: remove reducing altitude target while PILOT_TKOFF_ALT

--- ArduCopter/takeoff.cpp
+++ ArduCopter/takeoff.cpp
@@ -96,11 +96,6 @@ void Mode::_TakeOff::get_climb_rates(float& pilot_climb_rate, float& takeoff_cli
         return;
     }
 
-    // if pilot's commands descent, reduce altitude target
-    if (pilot_climb_rate < 0.0f) {
-        alt_delta_cm -= (pilot_climb_rate * dt);
-    }
-
     // acceleration of 50cm/s/s
     static constexpr float TAKEOFF_ACCEL_CMSS = 50.0f;
 
