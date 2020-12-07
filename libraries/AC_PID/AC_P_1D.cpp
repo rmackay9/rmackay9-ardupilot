lthall/ardupilot

AC_PID: library update and additional functions

libraries/AC_PID/AC_P_1D.cpp
@@ -1,0 +1,170 @@
+/// @file	AC_P_1D.cpp
+/// @brief	Generic P algorithm
+
+#include <AP_Math/AP_Math.h>
+#include "AC_P_1D.h"
+
+const AP_Param::GroupInfo AC_P_1D::var_info[] = {
+    // @Param: P
+    // @DisplayName: P Proportional Gain
+    // @Description: P Gain which produces an output value that is proportional to the current error value
+    AP_GROUPINFO("P",    0, AC_P_1D, _kp, 0),
+    AP_GROUPEND
+};
+
+// Constructor
+AC_P_1D::AC_P_1D(float initial_p, float dt) :
+    _dt(dt)
+{
+    // load parameter values from eeprom
+    AP_Param::setup_object_defaults(this, var_info);
+
+    _kp = initial_p;
+    _error = 0.0f;
+    _lim_err_neg = 0.0f;     // error value to enable filtering
+    _lim_err_pos = 0.0f;     // error value to enable filtering
+    _lim_out_neg = 0.0f;     // error value to enable filtering
+    _lim_out_pos = 0.0f;     // error value to enable filtering
+    _lim_D_Out = 10.0f;     // maximum first differential of output
+}
+
+// set_dt - set time step in seconds
+void AC_P_1D::set_dt(float dt)
+{
+    // set dt and calculate the input filter alpha
+    _dt = dt;
+}
+
+// set_dt - set time step in seconds
+void AC_P_1D::set_limits_error(float lim_err_neg, float lim_err_pos, float lim_out_neg, float lim_out_pos, float lim_D_Out, float lim_D2_Out)
+{
+    _lim_err_neg = -inv_sqrt_controller(lim_err_neg, _kp, _lim_D_Out);
+    _lim_err_pos = inv_sqrt_controller(lim_err_pos, _kp, _lim_D_Out);
+    _lim_out_neg = lim_out_neg;     // error value to enable filtering
+    _lim_out_pos = lim_out_pos;     // error value to enable filtering
+    _lim_D_Out = lim_D_Out;
+    if(is_positive(lim_D2_Out)) {
+        // limit the first derivative so as not to exceed the second derivative
+        _lim_D_Out = MIN(_lim_D_Out, lim_D2_Out / _kp);
+    }
+}
+
+//  update_all - set target and measured inputs to PID controller and calculate outputs
+//  target and _error are filtered
+//  the derivative is then calculated and filtered
+//  the integral is then updated based on the setting of the limit flag
+float AC_P_1D::update_all(float &target, float measurement, bool limit_min, bool limit_max)
+{
+    limit_min = false;
+    limit_max = false;
+
+    // calculate distance _error
+    float error = target - measurement;
+
+    if (asymetricLimit(error, _lim_err_neg, _lim_err_pos, limit_min, limit_max )) {
+        target = measurement + error;
+    }
+
+//    todo: Replace sqrt_controller with optimal acceleration and jerk limited curve
+    // MIN(_Dxy_max, _D2xy_max / _kxy_P) limits the max accel to the point where max jerk is exceeded
+    return sqrt_controller(error, _kp, _lim_D_Out, _dt);
+}
+
+float AC_P_1D::get_p() const
+{
+    return _error * _kp;
+}
+
+void AC_P_1D::load_gains()
+{
+    _kp.load();
+}
+
+void AC_P_1D::save_gains()
+{
+    _kp.save();
+}
+
+/// Overload the function call operator to permit easy initialisation
+void AC_P_1D::operator()(float initial_p, float dt)
+{
+    _kp = initial_p;
+    _dt = dt;
+}
+
+/// asymetricLimit - set limits based on seperate max and min
+bool AC_P_1D::asymetricLimit(float &input, float min, float max, bool &limitMin, bool &limitMax )
+{
+    if (input < min) {
+        input = min;
+        limitMin = true;
+    }
+    if (input > max) {
+        input = max;
+        limitMax = true;
+    }
+
+    return limitMin || limitMax;
+}
+
+// Proportional controller with piecewise sqrt sections to constrain second derivative
+float AC_P_1D::sqrt_controller(float error, float p, float D_max, float dt)
+{
+    float output;
+    if (!is_positive(D_max)) {
+        // second order limit is zero or negative.
+        output = error * p;
+    } else if (is_zero(p)) {
+        // P term is zero but we have a second order limit.
+        if (is_positive(error)) {
+            output = safe_sqrt(2.0f * D_max * (error));
+        } else if (is_negative(error)) {
+            output = -safe_sqrt(2.0f * D_max * (-error));
+        } else {
+            output = 0.0f;
+        }
+    } else {
+        // Both the P and second order limit have been defined.
+        float linear_dist = D_max / sq(p);
+        if (error > linear_dist) {
+            output = safe_sqrt(2.0f * D_max * (error - (linear_dist / 2.0f)));
+        } else if (error < -linear_dist) {
+            output = -safe_sqrt(2.0f * D_max * (-error - (linear_dist / 2.0f)));
+        } else {
+            output = error * p;
+        }
+    }
+    if (!is_zero(dt)) {
+        // this ensures we do not get small oscillations by over shooting the error correction in the last time step.
+        return constrain_float(output, -fabsf(error) / dt, fabsf(error) / dt);
+    } else {
+        return output;
+    }
+}
+
+// Proportional controller with piecewise sqrt sections to constrain second derivative
+float AC_P_1D::inv_sqrt_controller(float output, float p, float D_max)
+{
+    output = fabsf(output);
+
+    float error;
+    if (!is_positive(D_max) && is_zero(p)) {
+        error = 0.0f;
+    } else if (!is_positive(D_max)) {
+        // second order limit is zero or negative.
+        error = output / p;
+    } else if (is_zero(p)) {
+        // P term is zero but we have a second order limit.
+        error = sq(output)/(2*D_max);
+    } else {
+        // Both the P and second order limit have been defined.
+        float linear_out = D_max / p;
+        if (output > linear_out) {
+            error = sq(output)/(2*D_max) + D_max/(4*sq(p));
+        } else {
+            error = output / p;
+        }
+    }
+    return error;
+}
+

libraries/AC_PID/AC_P_2D.cpp
@@ -1,0 +1,110 @@
+/// @file   AC_P_2D.cpp
+/// @brief  Generic PID algorithm
+
+#include <AP_Math/AP_Math.h>
+#include "AC_P_2D.h"
+
+const AP_Param::GroupInfo AC_P_2D::var_info[] = {
+    // @Param: P
+    // @DisplayName: PI Proportional Gain
+    // @Description: P Gain which produces an output value that is proportional to the current error value
+    AP_GROUPINFO("P",    0, AC_P_2D, _kp, 0),
+    AP_GROUPEND
+};
+
+// Constructor
+AC_P_2D::AC_P_2D(float initial_p, float dt) :
+    _dt(dt)
+{
+    // load parameter values from eeprom
+    AP_Param::setup_object_defaults(this, var_info);
+
+    _D_max = 0.0f;
+    _D2_max = 0.0f;
+    _kp = initial_p;
+}
+
+// set_dt - set time step in seconds
+void AC_P_2D::set_dt(float dt)
+{
+    // set dt and calculate the input filter alpha
+    _dt = dt;
+}
+
+//  update_all - set target and measured inputs to PID controller and calculate outputs
+//  target and _error are filtered
+//  the derivative is then calculated and filtered
+//  the integral is then updated based on the setting of the limit flag
+Vector2f AC_P_2D::update_all(float &target_x, float &target_y, Vector2f measurement, float max)
+{
+    // calculate distance _error
+    _error = Vector2f(target_x, target_y) - measurement;
+
+    // Constrain _error and target position
+    // Constrain the maximum length of _vel_target to the maximum position correction velocity
+    // TODO: replace the leash length with a user definable maximum position correction
+    if (limit_vector_length(_error.x, _error.y, max)) {
+        target_x = measurement.x + _error.x;
+        target_y = measurement.y + _error.y;
+    }
+
+//    todo: Replace sqrt_controller with optimal acceleration and jerk limited curve
+    // MIN(_Dmax, _D2max / _kp) limits the max accel to the point where max jerk is exceeded
+    return sqrt_controller(Vector2f(_error.x, _error.y), _kp, MIN(_D_max, _D2_max / _kp), _dt);;
+}
+
+Vector2f AC_P_2D::get_p() const
+{
+    return _error * _kp;
+}
+
+/// Overload the function call operator to permit easy initialisation
+void AC_P_2D::operator()(float initial_p, float dt)
+{
+    _kp = initial_p;
+    _dt = dt;
+}
+
+/// limit vector to a given length, returns true if vector was limited
+bool AC_P_2D::limit_vector_length(float& vector_x, float& vector_y, float max_length)
+{
+    float vector_length = norm(vector_x, vector_y);
+    if ((vector_length > max_length) && is_positive(vector_length)) {
+        vector_x *= (max_length / vector_length);
+        vector_y *= (max_length / vector_length);
+        return true;
+    }
+    return false;
+}
+
+/// Proportional controller with piecewise sqrt sections to constrain second derivative
+Vector2f AC_P_2D::sqrt_controller(const Vector2f& _error, float p, float second_ord_lim, float dt)
+{
+    Vector2f correction;
+    if (second_ord_lim < 0.0f || is_zero(second_ord_lim) || is_zero(p)) {
+        correction = _error * p;
+    }
+
+    float _error_length = _error.length();
+    if (!is_positive(second_ord_lim)) {
+        // second order limit is zero or negative.
+        correction =  _error * p;
+    } else if (is_zero(p)) {
+        // P term is zero but we have a second order limit.
+        float first_order_scale = safe_sqrt(2.0f * second_ord_lim * _error_length) / _error_length;
+        correction =  _error * first_order_scale;
+    } else {
+        float linear_dist = second_ord_lim / sq(p);
+        if (_error_length > linear_dist) {
+            float first_order_scale = safe_sqrt(2.0f * second_ord_lim * (_error_length - (linear_dist * 0.5f))) / _error_length;
+            correction =  _error * first_order_scale;
+        } else {
+            correction =  _error * p;
+        }
+    }
+    if (!is_zero(dt)) {
+        // this ensures we do not get small oscillations by over shooting the error correction in the last time step.
+        limit_vector_length(correction.x, correction.y, _error_length / dt);
+    }
+    return correction;
+}

rmackay9/rmackay9-ardupilot

libraries/AC_PID/AC_P_1D.cpp
@@ -1,0 +1,171 @@
+/// @file	AC_P_1D.cpp
+/// @brief	Generic P algorithm
+
+#include <AP_Math/AP_Math.h>
+#include "AC_P_1D.h"
+
+const AP_Param::GroupInfo AC_P_1D::var_info[] = {
+    // @Param: P
+    // @DisplayName: P Proportional Gain
+    // @Description: P Gain which produces an output value that is proportional to the current error value
+    AP_GROUPINFO("P",    0, AC_P_1D, _kp, 0),
+    AP_GROUPEND
+};
+
+// Constructor
+AC_P_1D::AC_P_1D(float initial_p, float dt) :
+    _dt(dt)
+{
+    // load parameter values from eeprom
+    AP_Param::setup_object_defaults(this, var_info);
+
+    _kp = initial_p;
+    _error = 0.0f;
+    _lim_err_neg = 0.0f;     // error value to enable filtering
+    _lim_err_pos = 0.0f;     // error value to enable filtering
+    _lim_out_neg = 0.0f;     // error value to enable filtering
+    _lim_out_pos = 0.0f;     // error value to enable filtering
+    _lim_D_Out = 10.0f;     // maximum first differential of output
+}
+
+// set_dt - set time step in seconds
+void AC_P_1D::set_dt(float dt)
+{
+    // set dt and calculate the input filter alpha
+    _dt = dt;
+}
+
+// set_dt - set time step in seconds
+void AC_P_1D::set_limits_error(float lim_err_neg, float lim_err_pos, float lim_out_neg, float lim_out_pos, float lim_D_Out, float lim_D2_Out)
+{
+    _lim_err_neg = -inv_sqrt_controller(lim_err_neg, _kp, _lim_D_Out);
+    _lim_err_pos = inv_sqrt_controller(lim_err_pos, _kp, _lim_D_Out);
+    _lim_out_neg = lim_out_neg;     // error value to enable filtering
+    _lim_out_pos = lim_out_pos;     // error value to enable filtering
+    _lim_D_Out = lim_D_Out;
+    if(is_positive(lim_D2_Out)) {
+        // limit the first derivative so as not to exceed the second derivative
+        _lim_D_Out = MIN(_lim_D_Out, lim_D2_Out / _kp);
+    }
+}
+
+// update_all - set target and measured inputs to PID controller and calculate outputs
+// target and measurement are filtered
+// if measurement is further than error_min or error_max (see set_limits_error method)
+//   the target is moved closer to the measurement and limit_min or limit_max will be set true
+float AC_P_1D::update_all(float &target, float measurement, bool &limit_min, bool &limit_max)
+{
+    limit_min = false;
+    limit_max = false;
+
+    // calculate distance _error
+    float error = target - measurement;
+
+    if (asymetric_limit(error, _lim_err_neg, _lim_err_pos, limit_min, limit_max )) {
+        target = measurement + error;
+    }
+
+    // ToDo: Replace sqrt_controller with optimal acceleration and jerk limited curve
+    // MIN(_Dxy_max, _D2xy_max / _kxy_P) limits the max accel to the point where max jerk is exceeded
+    return sqrt_controller(error, _kp, _lim_D_Out, _dt);
+}
+
+float AC_P_1D::get_p() const
+{
+    return _error * _kp;
+}
+
+void AC_P_1D::load_gains()
+{
+    _kp.load();
+}
+
+void AC_P_1D::save_gains()
+{
+    _kp.save();
+}
+
+/// Overload the function call operator to permit easy initialisation
+void AC_P_1D::operator()(float initial_p, float dt)
+{
+    _kp = initial_p;
+    _dt = dt;
+}
+
+/// set limits based on separate min and max
+bool AC_P_1D::asymetric_limit(float &input, float min, float max, bool &limit_min, bool &limit_max )
+{
+    if (input < min) {
+        input = min;
+        limit_min = true;
+    }
+    if (input > max) {
+        input = max;
+        limit_max = true;
+    }
+
+    return limit_min || limit_max;
+}
+
+// Proportional controller with piecewise sqrt sections to constrain second derivative
+float AC_P_1D::sqrt_controller(float error, float p, float D_max, float dt)
+{
+    float output;
+    if (!is_positive(D_max)) {
+        // second order limit is zero or negative.
+        output = error * p;
+    } else if (is_zero(p)) {
+        // P term is zero but we have a second order limit.
+        if (is_positive(error)) {
+            output = safe_sqrt(2.0f * D_max * (error));
+        } else if (is_negative(error)) {
+            output = -safe_sqrt(2.0f * D_max * (-error));
+        } else {
+            output = 0.0f;
+        }
+    } else {
+        // both the P and second order limit have been defined.
+        float linear_dist = D_max / sq(p);
+        if (error > linear_dist) {
+            output = safe_sqrt(2.0f * D_max * (error - (linear_dist / 2.0f)));
+        } else if (error < -linear_dist) {
+            output = -safe_sqrt(2.0f * D_max * (-error - (linear_dist / 2.0f)));
+        } else {
+            output = error * p;
+        }
+    }
+    if (!is_zero(dt)) {
+        // this ensures we do not get small oscillations by over shooting the error correction in the last time step.
+        return constrain_float(output, -fabsf(error) / dt, fabsf(error) / dt);
+    }
+
+    return output;
+}
+
+// Proportional controller with piecewise sqrt sections to constrain second derivative
+float AC_P_1D::inv_sqrt_controller(float output, float p, float D_max)
+{
+    output = fabsf(output);
+
+    float error;
+    if (!is_positive(D_max) && is_zero(p)) {
+        error = 0.0f;
+    } else if (!is_positive(D_max)) {
+        // second order limit is zero or negative.
+        error = output / p;
+    } else if (is_zero(p)) {
+        // P term is zero but we have a second order limit.
+        error = sq(output)/(2*D_max);
+    } else {
+        // both the P and second order limit have been defined.
+        float linear_out = D_max / p;
+        if (output > linear_out) {
+            error = sq(output)/(2*D_max) + D_max/(4*sq(p));
+        } else {
+            error = output / p;
+        }
+    }
+
+    return error;
+}
+

libraries/AC_PID/AC_P_2D.cpp
@@ -1,0 +1,110 @@
+/// @file   AC_P_2D.cpp
+/// @brief  Generic PID algorithm
+
+#include <AP_Math/AP_Math.h>
+#include "AC_P_2D.h"
+
+const AP_Param::GroupInfo AC_P_2D::var_info[] = {
+    // @Param: P
+    // @DisplayName: PI Proportional Gain
+    // @Description: P Gain which produces an output value that is proportional to the current error value
+    AP_GROUPINFO("P",    0, AC_P_2D, _kp, 0),
+    AP_GROUPEND
+};
+
+// Constructor
+AC_P_2D::AC_P_2D(float initial_p, float dt) :
+    _dt(dt)
+{
+    // load parameter values from eeprom
+    AP_Param::setup_object_defaults(this, var_info);
+
+    _D_max = 0.0f;
+    _D2_max = 0.0f;
+    _kp = initial_p;
+}
+
+// set_dt - set time step in seconds
+void AC_P_2D::set_dt(float dt)
+{
+    // set dt and calculate the input filter alpha
+    _dt = dt;
+}
+
+//  update_all - set target and measured inputs to PID controller and calculate outputs
+//  target and _error are filtered
+//  the derivative is then calculated and filtered
+//  the integral is then updated based on the setting of the limit flag
+Vector2f AC_P_2D::update_all(float &target_x, float &target_y, Vector2f measurement, float error_max, float D2_max)
+{
+    // calculate distance _error
+    _error = Vector2f(target_x, target_y) - measurement;
+
+    // Constrain _error and target position
+    // Constrain the maximum length of _vel_target to the maximum position correction velocity
+    if (limit_vector_length(_error.x, _error.y, error_max)) {
+        target_x = measurement.x + _error.x;
+        target_y = measurement.y + _error.y;
+    }
+
+    // todo: Replace sqrt_controller with optimal acceleration and jerk limited curve
+    // MIN(_Dmax, _D2max / _kp) limits the max accel to the point where max jerk is exceeded
+    // return sqrt_controller(Vector2f(_error.x, _error.y), _kp, MIN(_D_max, _D2_max / _kp), _dt);
+    return sqrt_controller(Vector2f(_error.x, _error.y), _kp, D2_max, _dt);
+}
+
+Vector2f AC_P_2D::get_p() const
+{
+    return _error * _kp;
+}
+
+/// Overload the function call operator to permit easy initialisation
+void AC_P_2D::operator()(float initial_p, float dt)
+{
+    _kp = initial_p;
+    _dt = dt;
+}
+
+/// limit vector to a given length, returns true if vector was limited
+bool AC_P_2D::limit_vector_length(float& vector_x, float& vector_y, float max_length)
+{
+    float vector_length = norm(vector_x, vector_y);
+    if ((vector_length > max_length) && is_positive(vector_length)) {
+        vector_x *= (max_length / vector_length);
+        vector_y *= (max_length / vector_length);
+        return true;
+    }
+    return false;
+}
+
+/// Proportional controller with piecewise sqrt sections to constrain second derivative
+Vector2f AC_P_2D::sqrt_controller(const Vector2f& _error, float p, float second_ord_lim, float dt)
+{
+    Vector2f correction;
+    if (second_ord_lim < 0.0f || is_zero(second_ord_lim) || is_zero(p)) {
+        correction = _error * p;
+    }
+
+    float _error_length = _error.length();
+    if (!is_positive(second_ord_lim)) {
+        // second order limit is zero or negative.
+        correction =  _error * p;
+    } else if (is_zero(p)) {
+        // P term is zero but we have a second order limit.
+        float first_order_scale = safe_sqrt(2.0f * second_ord_lim * _error_length) / _error_length;
+        correction =  _error * first_order_scale;
+    } else {
+        float linear_dist = second_ord_lim / sq(p);
+        if (_error_length > linear_dist) {
+            float first_order_scale = safe_sqrt(2.0f * second_ord_lim * (_error_length - (linear_dist * 0.5f))) / _error_length;
+            correction =  _error * first_order_scale;
+        } else {
+            correction =  _error * p;
+        }
+    }
+    if (!is_zero(dt)) {
+        // this ensures we do not get small oscillations by over shooting the error correction in the last time step.
+        limit_vector_length(correction.x, correction.y, _error_length / dt);
+    }
+    return correction;
+}