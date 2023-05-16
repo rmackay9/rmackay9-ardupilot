18a8cede75eff3de2a64e20c758fb55227b12d5a
Randy Mackay <rmackay9@yahoo.com>
2023-05-16 20:27:03 +0900

    Filter: mode filter implemention for 3-element float
    # Conflicts:
    #	libraries/Filter/ModeFilter.cpp

--- libraries/Filter/ModeFilter.cpp
+++ libraries/Filter/ModeFilter.cpp
@@ -97,5 +97,6 @@ void ModeFilter<T,FILTER_SIZE>::isort(T new_sample, bool drop_high)
 
 // instantiate required implementations
 template class ModeFilter<float,5>;
+template class ModeFilter<float,3>;
 template class ModeFilter<int16_t,3>;
 template class ModeFilter<int16_t,5>;
