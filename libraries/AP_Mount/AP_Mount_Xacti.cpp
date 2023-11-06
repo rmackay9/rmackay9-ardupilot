2cb07d66fc1d4c587603f6a0e910b81ab8f3e32f
Randy Mackay <rmackay9@yahoo.com>
2023-11-06 14:01:52 +0900

    AP_Mount: Xacti take pic reliability improved

--- libraries/AP_Mount/AP_Mount_Xacti.cpp
+++ libraries/AP_Mount/AP_Mount_Xacti.cpp
@@ -18,18 +18,10 @@ UC_REGISTRY_BINDER(XactiGimbalAttitudeStatusCb, com::xacti::GimbalAttitudeStatus
 UC_REGISTRY_BINDER(XactiGnssStatusReqCb, com::xacti::GnssStatusReq);
 
 #define LOG_TAG "Mount"
-#define XACTI_PARAM_SINGLESHOT "SingleShot"
-#define XACTI_PARAM_RECORDING "Recording"
-#define XACTI_PARAM_FOCUSMODE "FocusMode"
-#define XACTI_PARAM_SENSORMODE "SensorMode"
-#define XACTI_PARAM_DIGITALZOOM "DigitalZoomMagnification"
-#define XACTI_PARAM_FIRMWAREVERSION "FirmwareVersion"
-#define XACTI_PARAM_STATUS "Status"
-#define XACTI_PARAM_DATETIME "DateTime"
-
 #define XACTI_MSG_SEND_MIN_MS 20                    // messages should not be sent to camera more often than 20ms
 #define XACTI_ZOOM_RATE_UPDATE_INTERVAL_MS  500     // zoom rate control increments zoom by 10% up or down every 0.5sec
 #define XACTI_STATUS_REQ_INTERVAL_MS 3000           // request status every 3 seconds
+#define XACTI_SET_PARAM_QUEUE_SIZE 3                // three set-param requests may be queued
 
 #define AP_MOUNT_XACTI_DEBUG 0
 #define debug(fmt, args ...) do { if (AP_MOUNT_XACTI_DEBUG) { GCS_SEND_TEXT(MAV_SEVERITY_INFO, "Xacti: " fmt, ## args); } } while (0)
@@ -39,6 +31,7 @@ AP_Mount_Xacti::DetectedModules AP_Mount_Xacti::_detected_modules[];
 HAL_Semaphore AP_Mount_Xacti::_sem_registry;
 const char* AP_Mount_Xacti::send_text_prefix = "Xacti:";
 const char* AP_Mount_Xacti::sensor_mode_str[] = { "RGB", "IR", "PIP", "NDVI" };
+const char* AP_Mount_Xacti::_param_names[] = {"SingleShot", "Recording", "FocusMode", "SensorMode", "DigitalZoomMagnification", "FirmwareVersion", "Status", "DateTime"};
 
 // Constructor
 AP_Mount_Xacti::AP_Mount_Xacti(class AP_Mount &frontend, class AP_Mount_Params &params, uint8_t instance) :
@@ -49,11 +42,20 @@ AP_Mount_Xacti::AP_Mount_Xacti(class AP_Mount &frontend, class AP_Mount_Params &
     param_int_cb = FUNCTOR_BIND_MEMBER(&AP_Mount_Xacti::handle_param_get_set_response_int, bool, AP_UAVCAN*, const uint8_t, const char*, int32_t &);
     param_string_cb = FUNCTOR_BIND_MEMBER(&AP_Mount_Xacti::handle_param_get_set_response_string, bool, AP_UAVCAN*, const uint8_t, const char*, AP_UAVCAN::string &);
     param_save_cb = FUNCTOR_BIND_MEMBER(&AP_Mount_Xacti::handle_param_save_response, void, AP_UAVCAN*, const uint8_t, bool);
+
+    // static assert that Param enum matches parameter names array
+    static_assert((uint8_t)AP_Mount_Xacti::Param::LAST+1 == ARRAY_SIZE(AP_Mount_Xacti::_param_names), "AP_Mount_Xacti::_param_names array must match Param enum");
 }
 
 // init - performs any required initialisation for this instance
 void AP_Mount_Xacti::init()
 {
+    // instantiate parameter queue, return on failure so init fails
+    _set_param_int32_queue = new ObjectArray<SetParamQueueItem>(XACTI_SET_PARAM_QUEUE_SIZE);
+    if (_set_param_int32_queue == nullptr) {
+        gcs().send_text(MAV_SEVERITY_ERROR, "%s init failed", send_text_prefix);
+        return;
+    }
     _initialised = true;
 }
 
@@ -83,6 +85,11 @@ void AP_Mount_Xacti::update()
 
     // request status
     if (request_status(now_ms)) {
+        return;
+    }
+
+    // process queue of set parameter items
+    if (process_set_param_int32_queue()) {
         return;
     }
 
@@ -191,19 +198,20 @@ bool AP_Mount_Xacti::healthy() const
 // take a picture.  returns true on success
 bool AP_Mount_Xacti::take_picture()
 {
-    if (_detected_modules[_instance].ap_dronecan == nullptr) {
+    // fail if camera errored
+    if (_camera_error) {
+        gcs().send_text(MAV_SEVERITY_ERROR, "%s unable to take pic", send_text_prefix);
         return false;
     }
 
-    // set SingleShot parameter
-    return set_param_int32(XACTI_PARAM_SINGLESHOT, 0);
+    return set_param_int32(Param::SingleShot, 0);
 }
 
 // start or stop video recording.  returns true on success
 // set start_recording = true to start record, false to stop recording
 bool AP_Mount_Xacti::record_video(bool start_recording)
 {
-    return set_param_int32(XACTI_PARAM_RECORDING, start_recording ? 1 : 0);
+    return set_param_int32(Param::Recording, start_recording ? 1 : 0);
 }
 
 // zoom in, out or hold
@@ -236,7 +244,7 @@ bool AP_Mount_Xacti::set_manual_focus_step(int8_t focus_step)
     uint8_t focus_param_value = 0;
 
     // set FocusMode parameter
-    return set_param_int32(XACTI_PARAM_FOCUSMODE, focus_param_value);
+    return set_param_int32(Param::FocusMode, focus_param_value);
 }
 
 // auto focus.  returns true on success
@@ -250,7 +258,7 @@ bool AP_Mount_Xacti::set_auto_focus()
     uint8_t focus_param_value = 2;
 
     // set FocusMode parameter
-    return _detected_modules[_instance].ap_dronecan->set_parameter_on_node(_detected_modules[_instance].node_id, XACTI_PARAM_FOCUSMODE, focus_param_value, &param_int_cb);
+    return set_param_int32(Param::FocusMode, focus_param_value);
 }
 
 // get attitude as a quaternion.  returns true on success
@@ -427,13 +435,13 @@ bool AP_Mount_Xacti::handle_param_get_set_response_int(AP_UAVCAN* ap_dronecan, u
 {
     // display errors
     const char* err_prefix_str = "Xacti: failed to";
-    if (strcmp(name, XACTI_PARAM_SINGLESHOT) == 0) {
+    if (strcmp(name, get_param_name_str(Param::SingleShot)) == 0) {
         if (value < 0) {
             gcs().send_text(MAV_SEVERITY_ERROR, "%s take pic", err_prefix_str);
         }
         return false;
     }
-    if (strcmp(name, XACTI_PARAM_RECORDING) == 0) {
+    if (strcmp(name, get_param_name_str(Param::Recording)) == 0) {
         if (value < 0) {
             _recording_video = false;
             gcs().send_text(MAV_SEVERITY_ERROR, "%s record", err_prefix_str);
@@ -443,7 +451,7 @@ bool AP_Mount_Xacti::handle_param_get_set_response_int(AP_UAVCAN* ap_dronecan, u
         }
         return false;
     }
-    if (strcmp(name, XACTI_PARAM_FOCUSMODE) == 0) {
+    if (strcmp(name, get_param_name_str(Param::FocusMode)) == 0) {
         if (value < 0) {
             gcs().send_text(MAV_SEVERITY_ERROR, "%s change focus", err_prefix_str);
         } else {
@@ -451,7 +459,7 @@ bool AP_Mount_Xacti::handle_param_get_set_response_int(AP_UAVCAN* ap_dronecan, u
         }
         return false;
     }
-    if (strcmp(name, XACTI_PARAM_SENSORMODE) == 0) {
+    if (strcmp(name, get_param_name_str(Param::SensorMode)) == 0) {
         if (value < 0) {
             gcs().send_text(MAV_SEVERITY_ERROR, "%s change lens", err_prefix_str);
         } else if ((uint32_t)value < ARRAY_SIZE(sensor_mode_str)) {
@@ -459,7 +467,7 @@ bool AP_Mount_Xacti::handle_param_get_set_response_int(AP_UAVCAN* ap_dronecan, u
         }
         return false;
     }
-    if (strcmp(name, XACTI_PARAM_DIGITALZOOM) == 0) {
+    if (strcmp(name, get_param_name_str(Param::DigitalZoomMagnification)) == 0) {
         if (value < 0) {
             gcs().send_text(MAV_SEVERITY_ERROR, "%s change zoom", err_prefix_str);
             // disable zoom rate control (if active) to avoid repeated failures
@@ -477,7 +485,7 @@ bool AP_Mount_Xacti::handle_param_get_set_response_int(AP_UAVCAN* ap_dronecan, u
 // handle param get/set response
 bool AP_Mount_Xacti::handle_param_get_set_response_string(AP_UAVCAN* ap_dronecan, uint8_t node_id, const char* name, AP_UAVCAN::string &value)
 {
-    if (strcmp(name, XACTI_PARAM_FIRMWAREVERSION) == 0) {
+    if (strcmp(name, get_param_name_str(Param::FirmwareVersion)) == 0) {
         _firmware_version.received = true;
         const uint8_t len = MIN(value.len, ARRAY_SIZE(_firmware_version.str)-1);
         memcpy(_firmware_version.str, (const char*)value.data, len);
@@ -496,11 +504,11 @@ bool AP_Mount_Xacti::handle_param_get_set_response_string(AP_UAVCAN* ap_dronecan
             _firmware_version.mav_ver = UINT32_VALUE(dev_ver_num, patch_ver_num, minor_ver_num, major_ver_num);
         }
         return false;
-    } else if (strcmp(name, XACTI_PARAM_DATETIME) == 0) {
+    } else if (strcmp(name, get_param_name_str(Param::DateTime)) == 0) {
         // display when time and date have been set
         gcs().send_text(MAV_SEVERITY_INFO, "%s datetime set %s", send_text_prefix, (const char*)value.data);
         return false;
-    } else if (strcmp(name, XACTI_PARAM_STATUS) == 0) {
+    } else if (strcmp(name, get_param_name_str(Param::Status)) == 0) {
         // check for expected length
         const char* error_str = "error";
         if (value.len != sizeof(_status)) {
@@ -516,7 +524,11 @@ bool AP_Mount_Xacti::handle_param_get_set_response_string(AP_UAVCAN* ap_dronecan
         uint32_t changed_bits = last_error_status ^ _status.error_status;
         const char* ok_str = "OK";
         if (changed_bits & (uint32_t)ErrorStatus::TIME_NOT_SET) {
-            gcs().send_text(MAV_SEVERITY_INFO, "%s time %sset", send_text_prefix, _status.error_status & (uint32_t)ErrorStatus::TIME_NOT_SET ? "not " : " ");
+            gcs().send_text(MAV_SEVERITY_INFO, "%s time %sset", send_text_prefix, _status.error_status & (uint32_t)ErrorStatus::TIME_NOT_SET ? "not " : "");
+            if (_status.error_status & (uint32_t)ErrorStatus::TIME_NOT_SET) {
+                // try to set time again
+                _datetime.set = false;
+            }
         }
         if (changed_bits & (uint32_t)ErrorStatus::MEDIA_ERROR) {
             gcs().send_text(MAV_SEVERITY_INFO, "%s media %s", send_text_prefix, _status.error_status & (uint32_t)ErrorStatus::MEDIA_ERROR ? error_str : ok_str);
@@ -539,6 +551,7 @@ bool AP_Mount_Xacti::handle_param_get_set_response_string(AP_UAVCAN* ap_dronecan
 
         // set motor error for health reporting
         _motor_error = _status.error_status & ((uint32_t)ErrorStatus::MOTOR_INIT_ERROR | (uint32_t)ErrorStatus::MOTOR_OPERATION_ERROR | (uint32_t)ErrorStatus::GIMBAL_CONTROL_ERROR);
+        _camera_error = _status.error_status & ((uint32_t)ErrorStatus::LENS_ERROR | (uint32_t)ErrorStatus::MEDIA_ERROR);
         return false;
     }
 
@@ -555,27 +568,42 @@ void AP_Mount_Xacti::handle_param_save_response(AP_UAVCAN* ap_dronecan, const ui
     }
 }
 
-// helper function to set integer parameters
-bool AP_Mount_Xacti::set_param_int32(const char* param_name, int32_t param_value)
+// get parameter name for a particular param enum value
+const char* AP_Mount_Xacti::get_param_name_str(Param param) const
 {
-    if (_detected_modules[_instance].ap_dronecan == nullptr) {
-        return false;
+    // check to avoid reading beyond end of array.  This should never happen
+    if ((uint8_t)param > ARRAY_SIZE(_param_names)) {
+        INTERNAL_ERROR(AP_InternalError::error_t::invalid_arg_or_result);
+        return nullptr;
     }
-
-    if (_detected_modules[_instance].ap_dronecan->set_parameter_on_node(_detected_modules[_instance].node_id, param_name, param_value, &param_int_cb)) {
-        last_send_getset_param_ms = AP_HAL::millis();
-        return true;
-    }
-    return false;
+    return _param_names[(uint8_t)param];
 }
 
-bool AP_Mount_Xacti::set_param_string(const char* param_name, const AP_UAVCAN::string& param_value)
+// helper function to set integer parameters
+bool AP_Mount_Xacti::set_param_int32(Param param, int32_t param_value)
+{
+    if (_set_param_int32_queue == nullptr) {
+        return false;
+    }
+
+    // set param request added to queue to be sent.  throttling requests improves reliability
+    return _set_param_int32_queue->push(SetParamQueueItem{param, param_value});
+}
+
+// helper function to set string parameters
+bool AP_Mount_Xacti::set_param_string(Param param, const AP_UAVCAN::string& param_value)
 {
     if (_detected_modules[_instance].ap_dronecan == nullptr) {
         return false;
     }
 
-    if (_detected_modules[_instance].ap_dronecan->set_parameter_on_node(_detected_modules[_instance].node_id, param_name, param_value, &param_string_cb)) {
+    // convert param to string
+    const char* param_name_str = get_param_name_str(param);
+    if (param_name_str == nullptr) {
+        return false;
+    }
+
+    if (_detected_modules[_instance].ap_dronecan->set_parameter_on_node(_detected_modules[_instance].node_id, param_name_str, param_value, &param_string_cb)) {
         last_send_getset_param_ms = AP_HAL::millis();
         return true;
     }
@@ -583,15 +611,44 @@ bool AP_Mount_Xacti::set_param_string(const char* param_name, const AP_UAVCAN::s
 }
 
 // helper function to get string parameters
-bool AP_Mount_Xacti::get_param_string(const char* param_name)
+bool AP_Mount_Xacti::get_param_string(Param param)
 {
     if (_detected_modules[_instance].ap_dronecan == nullptr) {
         return false;
     }
 
-    if (_detected_modules[_instance].ap_dronecan->get_parameter_on_node(_detected_modules[_instance].node_id, param_name, &param_string_cb)) {
+    // convert param to string
+    const char* param_name_str = get_param_name_str(param);
+    if (param_name_str == nullptr) {
+        return false;
+    }
+
+    if (_detected_modules[_instance].ap_dronecan->get_parameter_on_node(_detected_modules[_instance].node_id, param_name_str, &param_string_cb)) {
         last_send_getset_param_ms = AP_HAL::millis();
         return true;
+    }
+    return false;
+}
+
+// process queue of set parameter items
+bool AP_Mount_Xacti::process_set_param_int32_queue()
+{
+    if ((_set_param_int32_queue == nullptr) || (_detected_modules[_instance].ap_dronecan == nullptr)) {
+        return false;
+    }
+
+    SetParamQueueItem param_to_set;
+    if (_set_param_int32_queue->pop(param_to_set)) {
+        // convert param to string
+        const char* param_name_str = get_param_name_str(param_to_set.param);
+        if (param_name_str == nullptr) {
+            return false;
+        }
+        if (_detected_modules[_instance].ap_dronecan->set_parameter_on_node(_detected_modules[_instance].node_id, param_name_str, param_to_set.value, &param_int_cb)) {
+            last_send_getset_param_ms = AP_HAL::millis();
+            return true;
+        }
+        return false;
     }
     return false;
 }
@@ -663,7 +720,7 @@ bool AP_Mount_Xacti::update_zoom_rate_control(uint32_t now_ms)
     }
 
     // send desired zoom to camera
-    return set_param_int32(XACTI_PARAM_DIGITALZOOM, zoom_value);
+    return set_param_int32(Param::DigitalZoomMagnification, zoom_value);
 }
 
 // request firmware version. now_ms should be current system time (reduces calls to AP_HAL::millis)
@@ -680,7 +737,7 @@ bool AP_Mount_Xacti::request_firmware_version(uint32_t now_ms)
         return false;
     }
     _firmware_version.last_request_ms = now_ms;
-    return get_param_string(XACTI_PARAM_FIRMWAREVERSION);
+    return get_param_string(Param::FirmwareVersion);
 }
 
 // set date and time.  now_ms is current system time
@@ -715,7 +772,7 @@ bool AP_Mount_Xacti::set_datetime(uint32_t now_ms)
         return false;
     }
     datetime_string.len = num_bytes;
-    _datetime.set = set_param_string(XACTI_PARAM_DATETIME, datetime_string);
+    _datetime.set = set_param_string(Param::DateTime, datetime_string);
     if (!_datetime.set) {
         gcs().send_text(MAV_SEVERITY_ERROR, "%s failed to set date/time", send_text_prefix);
     }
@@ -733,7 +790,7 @@ bool AP_Mount_Xacti::request_status(uint32_t now_ms)
     }
 
     _status_report.last_request_ms = now_ms;
-    return get_param_string(XACTI_PARAM_STATUS);
+    return get_param_string(Param::Status);
 }
 
 // check if safe to send message (if messages sent too often camera will not respond)
