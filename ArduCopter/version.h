1f10062eca35146bf895a78ddb1f7ae44ff20b50
Mad Max <poruchik111@gmail.com>
2024-02-21 12:37:42 +0200

    Changes for fpv 7

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.6.0-dev"
+#define THISFIRMWARE "Little Bro V4.6.0-dev"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,6,0,FIRMWARE_VERSION_TYPE_DEV
