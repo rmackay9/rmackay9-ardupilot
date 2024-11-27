c72b632b17b7c32830fab3dc65b09303d0b67416
max <poruchik111@gmail.com>
2024-11-27 21:03:02 +0200

    LB trouble&

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -273,13 +273,9 @@ void Copter::get_scheduler_tasks(const AP_Scheduler::Task *&tasks,
 constexpr int8_t Copter::_failsafe_priorities[7];
 
 
-<<<<<<< HEAD
 //#if AP_SCRIPTING_ENABLED || AP_EXTERNAL_CONTROL_ENABLED
-#if MODE_GUIDED_ENABLED == ENABLED
-=======
 #if AP_SCRIPTING_ENABLED || AP_EXTERNAL_CONTROL_ENABLED
 #if MODE_GUIDED_ENABLED
->>>>>>> upstream/master
 // set target location (for use by external control and scripting)
 bool Copter::set_target_location(const Location& target_loc)
 {
