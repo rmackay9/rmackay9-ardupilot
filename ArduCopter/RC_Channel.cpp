88cdb1901b48e0fa5c26b04d0acbd5d3c43c41da
max <poruchik111@gmail.com>
2025-10-22 16:35:28 +0300

    LB repaired

--- ArduCopter/RC_Channel.cpp
+++ ArduCopter/RC_Channel.cpp
@@ -211,9 +211,9 @@ bool RC_Channel_Copter::do_aux_function(const AUX_FUNC ch_option, const AuxSwitc
                     copter.set_mode(Mode::Number::LAND, ModeReason::RC_COMMAND);
                     break;
                 case AuxSwitchPos::MIDDLE:
-                source_setted = AP_NavEKF_Source::SourceSetSelection::PRIMARY;
-                AP::ahrs().set_posvelyaw_source_set(source_setted); 
-                copter.set_mode(Mode::Number::POSHOLD, ModeReason::RC_COMMAND);
+                //source_setted = AP_NavEKF_Source::SourceSetSelection::PRIMARY;
+                //AP::ahrs().set_posvelyaw_source_set(source_setted); 
+                //copter.set_mode(Mode::Number::POSHOLD, ModeReason::RC_COMMAND);
                     break;
                 case AuxSwitchPos::HIGH:
                     source_setted = AP_NavEKF_Source::SourceSetSelection::PRIMARY;
@@ -369,9 +369,9 @@ bool RC_Channel_Copter::do_aux_function(const AUX_FUNC ch_option, const AuxSwitc
                     copter.set_mode(Mode::Number::ALT_HOLD, ModeReason::RC_COMMAND);
                     break;
                 case AuxSwitchPos::HIGH:
-                    source_setted = AP_NavEKF_Source::SourceSetSelection::SECONDARY;
+                    source_setted = AP_NavEKF_Source::SourceSetSelection::PRIMARY;
                     AP::ahrs().set_posvelyaw_source_set(source_setted); 
-                    copter.set_mode(Mode::Number::FLOWHOLD, ModeReason::RC_COMMAND);
+                    copter.set_mode(Mode::Number::POSHOLD, ModeReason::RC_COMMAND);
                     break;
                 }
             break;
