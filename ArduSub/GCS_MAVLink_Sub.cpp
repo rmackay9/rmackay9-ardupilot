ArduPilot/ardupilot

Sub: NEU to NED renaming - No compiler change

ArduSub/GCS_MAVLink_Sub.cpp
@@ -219,7 +219,7 @@ void GCS_MAVLINK_Sub::send_pid_tuning()
         }
     }
     if (g.gcs_pid_mask & 8) {
-        const AP_PIDInfo &pid_info = sub.pos_control.get_accel_U_pid().get_pid_info();
+        const AP_PIDInfo &pid_info = sub.pos_control.get_accel_D_pid().get_pid_info();
         mavlink_msg_pid_tuning_send(chan, PID_TUNING_ACCZ,
                                     pid_info.target*0.01f,
                                     -(ahrs.get_accel_ef().z + GRAVITY_MSS),

ArduSub/Sub.cpp
@@ -232,7 +232,7 @@ void Sub::ten_hz_logging_loop()
             logger.Write_PID(LOG_PIDR_MSG, attitude_control.get_rate_roll_pid().get_pid_info());
             logger.Write_PID(LOG_PIDP_MSG, attitude_control.get_rate_pitch_pid().get_pid_info());
             logger.Write_PID(LOG_PIDY_MSG, attitude_control.get_rate_yaw_pid().get_pid_info());
-            logger.Write_PID(LOG_PIDA_MSG, pos_control.get_accel_U_pid().get_pid_info());
+            logger.Write_PID(LOG_PIDA_MSG, pos_control.get_accel_D_pid().get_pid_info());
         }
     }
     if (should_log(MASK_LOG_MOTBATT)) {
@@ -269,7 +269,7 @@ void Sub::twentyfive_hz_logging()
             logger.Write_PID(LOG_PIDR_MSG, attitude_control.get_rate_roll_pid().get_pid_info());
             logger.Write_PID(LOG_PIDP_MSG, attitude_control.get_rate_pitch_pid().get_pid_info());
             logger.Write_PID(LOG_PIDY_MSG, attitude_control.get_rate_yaw_pid().get_pid_info());
-            logger.Write_PID(LOG_PIDA_MSG, pos_control.get_accel_U_pid().get_pid_info());
+            logger.Write_PID(LOG_PIDA_MSG, pos_control.get_accel_D_pid().get_pid_info());
         }
     }
 
@@ -342,7 +342,7 @@ void Sub::one_hz_loop()
     set_likely_flying(hal.util->get_soft_armed());
 
     attitude_control.set_notch_sample_rate(AP::scheduler().get_filtered_loop_rate_hz());
-    pos_control.get_accel_U_pid().set_notch_sample_rate(AP::scheduler().get_filtered_loop_rate_hz());
+    pos_control.get_accel_D_pid().set_notch_sample_rate(AP::scheduler().get_filtered_loop_rate_hz());
 }
 
 void Sub::read_AHRS()

ArduSub/mode_auto.cpp
@@ -148,7 +148,7 @@ void ModeAuto::auto_wp_run()
 
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
-    position_control->update_U_controller();
+    position_control->update_D_controller();
 
     ////////////////////////////
     // update attitude output //
@@ -186,17 +186,17 @@ void ModeAuto::auto_circle_movetoedge_start(const Location &circle_center, float
     sub.circle_nav.set_rate_degs(current_rate);
 
     // check our distance from edge of circle
-    Vector3f circle_edge_neu;
+    Vector3f circle_edge_neu_cm;
     float dist_to_edge;
-    sub.circle_nav.get_closest_point_on_circle_NEU_cm(circle_edge_neu, dist_to_edge);
+    sub.circle_nav.get_closest_point_on_circle_NEU_cm(circle_edge_neu_cm, dist_to_edge);
 
     // if more than 3m then fly to edge
     if (dist_to_edge > 300.0f) {
         // set the state to move to the edge of the circle
         sub.auto_mode = Auto_CircleMoveToEdge;
 
-        // convert circle_edge_neu to Location
-        Location circle_edge(circle_edge_neu, Location::AltFrame::ABOVE_ORIGIN);
+        // convert circle_edge_neu_cm to Location
+        Location circle_edge(circle_edge_neu_cm, Location::AltFrame::ABOVE_ORIGIN);
 
         // convert altitude to same as command
         circle_edge.copy_alt_from(circle_center);
@@ -246,7 +246,7 @@ void ModeAuto::auto_circle_run()
 
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
-    position_control->update_U_controller();
+    position_control->update_D_controller();
 
     // roll & pitch from waypoint controller, yaw rate from pilot
     attitude_control->input_euler_angle_roll_pitch_yaw_cd(channel_roll->get_control_in(), channel_pitch->get_control_in(), sub.circle_nav.get_yaw_cd(), true);
@@ -332,7 +332,7 @@ void ModeAuto::auto_loiter_run()
 
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
-    position_control->update_U_controller();
+    position_control->update_D_controller();
 
     // get pilot desired lean angles
     float target_roll, target_pitch;
@@ -455,11 +455,11 @@ bool ModeAuto::auto_terrain_recover_start()
     sub.loiter_nav.init_target();
 
     // Reset z axis controller
-    position_control->relax_U_controller(motors.get_throttle_hover());
+    position_control->relax_D_controller(motors.get_throttle_hover());
 
     // initialize vertical maximum speeds and acceleration
-    position_control->set_max_speed_accel_U_cm(sub.wp_nav.get_default_speed_down_cms(), sub.wp_nav.get_default_speed_up_cms(), sub.wp_nav.get_accel_U_cmss());
-    position_control->set_correction_speed_accel_U_cm(sub.wp_nav.get_default_speed_down_cms(), sub.wp_nav.get_default_speed_up_cms(), sub.wp_nav.get_accel_U_cmss());
+    position_control->set_max_speed_accel_U_cm(sub.wp_nav.get_default_speed_down_cms(), sub.wp_nav.get_default_speed_up_cms(), sub.wp_nav.get_accel_D_cmss());
+    position_control->set_correction_speed_accel_U_cm(sub.wp_nav.get_default_speed_down_cms(), sub.wp_nav.get_default_speed_up_cms(), sub.wp_nav.get_accel_D_cmss());
 
     gcs().send_text(MAV_SEVERITY_WARNING, "Attempting auto failsafe recovery");
     return true;
@@ -481,8 +481,8 @@ void ModeAuto::auto_terrain_recover_run()
         attitude_control->set_throttle_out(0,true,g.throttle_filt);
         attitude_control->relax_attitude_controllers();
 
-        sub.loiter_nav.init_target();                                                   // Reset xy target
-        position_control->relax_U_controller(motors.get_throttle_hover());                // Reset z axis controller
+        sub.loiter_nav.init_target();                                       // Reset xy target
+        position_control->relax_D_controller(motors.get_throttle_hover());  // Reset z axis controller
         return;
     }
 
@@ -509,7 +509,7 @@ void ModeAuto::auto_terrain_recover_run()
             // Start timer as soon as rangefinder is healthy
             if (rangefinder_recovery_ms == 0) {
                 rangefinder_recovery_ms = AP_HAL::millis();
-                position_control->relax_U_controller(motors.get_throttle_hover()); // Reset alt hold targets
+                position_control->relax_D_controller(motors.get_throttle_hover()); // Reset alt hold targets
             }
 
             // 1.5 seconds of healthy rangefinder means we can resume mission with terrain enabled
@@ -561,7 +561,7 @@ void ModeAuto::auto_terrain_recover_run()
     /////////////////////
     // update z target //
     position_control->set_pos_target_U_from_climb_rate_cms(target_climb_rate);
-    position_control->update_U_controller();
+    position_control->update_D_controller();
 
     ////////////////////////////
     // update angular targets //

rmackay9/rmackay9-ardupilot

ArduSub/GCS_MAVLink_Sub.cpp
@@ -219,7 +219,7 @@ void GCS_MAVLINK_Sub::send_pid_tuning()
         }
     }
     if (g.gcs_pid_mask & 8) {
-        const AP_PIDInfo &pid_info = sub.pos_control.get_accel_U_pid().get_pid_info();
+        const AP_PIDInfo &pid_info = sub.pos_control.D_get_accel_pid().get_pid_info();
         mavlink_msg_pid_tuning_send(chan, PID_TUNING_ACCZ,
                                     pid_info.target*0.01f,
                                     -(ahrs.get_accel_ef().z + GRAVITY_MSS),

ArduSub/Sub.cpp
@@ -232,7 +232,7 @@ void Sub::ten_hz_logging_loop()
             logger.Write_PID(LOG_PIDR_MSG, attitude_control.get_rate_roll_pid().get_pid_info());
             logger.Write_PID(LOG_PIDP_MSG, attitude_control.get_rate_pitch_pid().get_pid_info());
             logger.Write_PID(LOG_PIDY_MSG, attitude_control.get_rate_yaw_pid().get_pid_info());
-            logger.Write_PID(LOG_PIDA_MSG, pos_control.get_accel_U_pid().get_pid_info());
+            logger.Write_PID(LOG_PIDA_MSG, pos_control.D_get_accel_pid().get_pid_info());
         }
     }
     if (should_log(MASK_LOG_MOTBATT)) {
@@ -269,7 +269,7 @@ void Sub::twentyfive_hz_logging()
             logger.Write_PID(LOG_PIDR_MSG, attitude_control.get_rate_roll_pid().get_pid_info());
             logger.Write_PID(LOG_PIDP_MSG, attitude_control.get_rate_pitch_pid().get_pid_info());
             logger.Write_PID(LOG_PIDY_MSG, attitude_control.get_rate_yaw_pid().get_pid_info());
-            logger.Write_PID(LOG_PIDA_MSG, pos_control.get_accel_U_pid().get_pid_info());
+            logger.Write_PID(LOG_PIDA_MSG, pos_control.D_get_accel_pid().get_pid_info());
         }
     }
 
@@ -342,7 +342,7 @@ void Sub::one_hz_loop()
     set_likely_flying(hal.util->get_soft_armed());
 
     attitude_control.set_notch_sample_rate(AP::scheduler().get_filtered_loop_rate_hz());
-    pos_control.get_accel_U_pid().set_notch_sample_rate(AP::scheduler().get_filtered_loop_rate_hz());
+    pos_control.D_get_accel_pid().set_notch_sample_rate(AP::scheduler().get_filtered_loop_rate_hz());
 }
 
 void Sub::read_AHRS()

ArduSub/mode_auto.cpp
@@ -148,7 +148,7 @@ void ModeAuto::auto_wp_run()
 
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
-    position_control->update_U_controller();
+    position_control->D_update_controller();
 
     ////////////////////////////
     // update attitude output //
@@ -186,17 +186,17 @@ void ModeAuto::auto_circle_movetoedge_start(const Location &circle_center, float
     sub.circle_nav.set_rate_degs(current_rate);
 
     // check our distance from edge of circle
-    Vector3f circle_edge_neu;
+    Vector3f circle_edge_neu_cm;
     float dist_to_edge;
-    sub.circle_nav.get_closest_point_on_circle_NEU_cm(circle_edge_neu, dist_to_edge);
+    sub.circle_nav.get_closest_point_on_circle_NEU_cm(circle_edge_neu_cm, dist_to_edge);
 
     // if more than 3m then fly to edge
     if (dist_to_edge > 300.0f) {
         // set the state to move to the edge of the circle
         sub.auto_mode = Auto_CircleMoveToEdge;
 
-        // convert circle_edge_neu to Location
-        Location circle_edge(circle_edge_neu, Location::AltFrame::ABOVE_ORIGIN);
+        // convert circle_edge_neu_cm to Location
+        Location circle_edge(circle_edge_neu_cm, Location::AltFrame::ABOVE_ORIGIN);
 
         // convert altitude to same as command
         circle_edge.copy_alt_from(circle_center);
@@ -246,7 +246,7 @@ void ModeAuto::auto_circle_run()
 
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
-    position_control->update_U_controller();
+    position_control->D_update_controller();
 
     // roll & pitch from waypoint controller, yaw rate from pilot
     attitude_control->input_euler_angle_roll_pitch_yaw_cd(channel_roll->get_control_in(), channel_pitch->get_control_in(), sub.circle_nav.get_yaw_cd(), true);
@@ -332,7 +332,7 @@ void ModeAuto::auto_loiter_run()
 
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
-    position_control->update_U_controller();
+    position_control->D_update_controller();
 
     // get pilot desired lean angles
     float target_roll, target_pitch;
@@ -455,12 +455,12 @@ bool ModeAuto::auto_terrain_recover_start()
     sub.loiter_nav.init_target();
 
     // Reset z axis controller
-    position_control->relax_U_controller(motors.get_throttle_hover());
+    position_control->D_relax_controller(motors.get_throttle_hover());
 
     // initialize vertical maximum speeds and acceleration
     // All limits must be positive
-    position_control->set_max_speed_accel_U_cm(sub.wp_nav.get_default_speed_down_cms(), sub.wp_nav.get_default_speed_up_cms(), sub.wp_nav.get_accel_U_cmss());
-    position_control->set_correction_speed_accel_U_cm(sub.wp_nav.get_default_speed_down_cms(), sub.wp_nav.get_default_speed_up_cms(), sub.wp_nav.get_accel_U_cmss());
+    position_control->D_set_max_speed_accel_cm(sub.wp_nav.get_default_speed_down_cms(), sub.wp_nav.get_default_speed_up_cms(), sub.wp_nav.get_accel_D_cmss());
+    position_control->D_set_correction_speed_accel_cm(sub.wp_nav.get_default_speed_down_cms(), sub.wp_nav.get_default_speed_up_cms(), sub.wp_nav.get_accel_D_cmss());
 
     gcs().send_text(MAV_SEVERITY_WARNING, "Attempting auto failsafe recovery");
     return true;
@@ -482,8 +482,8 @@ void ModeAuto::auto_terrain_recover_run()
         attitude_control->set_throttle_out(0,true,g.throttle_filt);
         attitude_control->relax_attitude_controllers();
 
-        sub.loiter_nav.init_target();                                                   // Reset xy target
-        position_control->relax_U_controller(motors.get_throttle_hover());                // Reset z axis controller
+        sub.loiter_nav.init_target();                                       // Reset xy target
+        position_control->D_relax_controller(motors.get_throttle_hover());  // Reset z axis controller
         return;
     }
 
@@ -510,7 +510,7 @@ void ModeAuto::auto_terrain_recover_run()
             // Start timer as soon as rangefinder is healthy
             if (rangefinder_recovery_ms == 0) {
                 rangefinder_recovery_ms = AP_HAL::millis();
-                position_control->relax_U_controller(motors.get_throttle_hover()); // Reset alt hold targets
+                position_control->D_relax_controller(motors.get_throttle_hover()); // Reset alt hold targets
             }
 
             // 1.5 seconds of healthy rangefinder means we can resume mission with terrain enabled
@@ -561,8 +561,8 @@ void ModeAuto::auto_terrain_recover_run()
 
     /////////////////////
     // update z target //
-    position_control->set_pos_target_U_from_climb_rate_cms(target_climb_rate);
-    position_control->update_U_controller();
+    position_control->D_set_pos_target_from_climb_rate_cms(target_climb_rate);
+    position_control->D_update_controller();
 
     ////////////////////////////
     // update angular targets //