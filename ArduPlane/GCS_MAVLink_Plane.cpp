ArduPilot/ardupilot

Plane: NEU to NED renaming - No compiler change

ArduPlane/GCS_MAVLink_Plane.cpp
@@ -212,7 +212,7 @@ void GCS_MAVLINK_Plane::send_nav_controller_output() const
         const Vector3f &targets = quadplane.attitude_control->get_att_target_euler_cd();
 
         const Vector2f& curr_pos_m = quadplane.inertial_nav.get_position_xy_cm() * 0.01;
-        const Vector2f& target_pos_m = quadplane.pos_control->get_pos_target_NEU_m().xy().tofloat();
+        const Vector2f& target_pos_m = quadplane.pos_control->get_pos_target_NED_m().xy().tofloat();
         const Vector2f error_m = (target_pos_m - curr_pos_m);
 
         mavlink_msg_nav_controller_output_send(
@@ -222,7 +222,7 @@ void GCS_MAVLINK_Plane::send_nav_controller_output() const
             targets.z * 0.01,
             degrees(error_m.angle()),
             MIN(error_m.length(), UINT16_MAX),
-            (plane.control_mode != &plane.mode_qstabilize) ? quadplane.pos_control->get_pos_error_U_m() : 0,
+            (plane.control_mode != &plane.mode_qstabilize) ? quadplane.pos_control->get_pos_error_D_m() : 0,
             plane.airspeed_error * 100,  // incorrect units; see PR#7933
             quadplane.wp_nav->crosstrack_error_m());
         return;
@@ -397,7 +397,7 @@ void GCS_MAVLINK_Plane::send_pid_tuning()
     }
 #if HAL_QUADPLANE_ENABLED
     if (g.gcs_pid_mask & TUNING_BITS_ACCZ && plane.quadplane.in_vtol_mode()) {
-        pid_info = &plane.quadplane.pos_control->get_accel_U_pid().get_pid_info();
+        pid_info = &plane.quadplane.pos_control->get_accel_D_pid().get_pid_info();
         send_pid_info(pid_info, PID_TUNING_ACCZ, pid_info->actual);
     }
 #endif
@@ -1232,7 +1232,7 @@ int16_t GCS_MAVLINK_Plane::high_latency_target_altitude() const
     const QuadPlane &quadplane = plane.quadplane;
     //return units are m
     if (quadplane.show_vtol_view()) {
-        return (plane.control_mode != &plane.mode_qstabilize) ? (global_position_current.alt * 0.01 + quadplane.pos_control->get_pos_error_U_m()) : 0;
+        return (plane.control_mode != &plane.mode_qstabilize) ? (global_position_current.alt * 0.01 + quadplane.pos_control->get_pos_error_D_m()) : 0;
     }
 #endif
     return 0.01 * (global_position_current.alt + plane.calc_altitude_error_cm());

ArduPlane/VTOL_Assist.cpp
@@ -182,7 +182,7 @@ bool VTOL_Assist::check_VTOL_recovery(void)
                    controller may limit pitch after a strong
                    acceleration event
                 */
-                quadplane.pos_control->init_U_controller();
+                quadplane.pos_control->init_D_controller();
                 quadplane.pos_control->init_NE_controller();
             }
         }

ArduPlane/mode_qhover.cpp
@@ -6,8 +6,8 @@
 bool ModeQHover::_enter()
 {
     // set vertical speed and acceleration limits
-    pos_control->set_max_speed_accel_U_m(quadplane.get_pilot_velocity_z_max_dn_m(), quadplane.pilot_speed_z_max_up_ms, quadplane.pilot_accel_z_mss);
-    pos_control->set_correction_speed_accel_U_m(quadplane.get_pilot_velocity_z_max_dn_m(), quadplane.pilot_speed_z_max_up_ms, quadplane.pilot_accel_z_mss);
+    pos_control->set_max_speed_accel_D_m(quadplane.get_pilot_velocity_z_max_dn_m(), quadplane.pilot_speed_z_max_up_ms, quadplane.pilot_accel_z_mss);
+    pos_control->set_correction_speed_accel_D_m(quadplane.get_pilot_velocity_z_max_dn_m(), quadplane.pilot_speed_z_max_up_ms, quadplane.pilot_accel_z_mss);
     quadplane.set_climb_rate_ms(0);
 
     quadplane.init_throttle_wait();
@@ -37,7 +37,7 @@ void ModeQHover::run()
         quadplane.set_desired_spool_state(AP_Motors::DesiredSpoolState::GROUND_IDLE);
         attitude_control->set_throttle_out(0, true, 0);
         quadplane.relax_attitude_control();
-        pos_control->relax_U_controller(0);
+        pos_control->relax_D_controller(0);
     } else {
         plane.quadplane.assign_tilt_to_fwd_thr();
         quadplane.hold_hover(quadplane.get_pilot_desired_climb_rate_cms());

ArduPlane/qautotune.cpp
@@ -41,10 +41,10 @@ void QAutoTune::get_pilot_desired_rp_yrate_rad(float &des_roll_rad, float &des_p
 void QAutoTune::init_z_limits()
 {
     // set vertical speed and acceleration limits
-    plane.quadplane.pos_control->set_max_speed_accel_U_m(plane.quadplane.get_pilot_velocity_z_max_dn_m(),
+    plane.quadplane.pos_control->set_max_speed_accel_D_m(plane.quadplane.get_pilot_velocity_z_max_dn_m(),
                                                        plane.quadplane.pilot_speed_z_max_up_ms,
                                                        plane.quadplane.pilot_accel_z_mss);
-    plane.quadplane.pos_control->set_correction_speed_accel_U_m(plane.quadplane.get_pilot_velocity_z_max_dn_m(),
+    plane.quadplane.pos_control->set_correction_speed_accel_D_m(plane.quadplane.get_pilot_velocity_z_max_dn_m(),
                                                               plane.quadplane.pilot_speed_z_max_up_ms,
                                                               plane.quadplane.pilot_accel_z_mss);
 }

rmackay9/rmackay9-ardupilot

ArduPlane/GCS_MAVLink_Plane.cpp
@@ -212,7 +212,7 @@ void GCS_MAVLINK_Plane::send_nav_controller_output() const
         const Vector3f &targets = quadplane.attitude_control->get_att_target_euler_cd();
 
         const Vector2f& curr_pos_m = quadplane.inertial_nav.get_position_xy_cm() * 0.01;
-        const Vector2f& target_pos_m = quadplane.pos_control->get_pos_target_NEU_m().xy().tofloat();
+        const Vector2f& target_pos_m = quadplane.pos_control->get_pos_target_NED_m().xy().tofloat();
         const Vector2f error_m = (target_pos_m - curr_pos_m);
 
         mavlink_msg_nav_controller_output_send(
@@ -222,7 +222,7 @@ void GCS_MAVLINK_Plane::send_nav_controller_output() const
             targets.z * 0.01,
             degrees(error_m.angle()),
             MIN(error_m.length(), UINT16_MAX),
-            (plane.control_mode != &plane.mode_qstabilize) ? quadplane.pos_control->get_pos_error_U_m() : 0,
+            (plane.control_mode != &plane.mode_qstabilize) ? quadplane.pos_control->get_pos_error_D_m() : 0,
             plane.airspeed_error * 100,  // incorrect units; see PR#7933
             quadplane.wp_nav->crosstrack_error_m());
         return;
@@ -397,7 +397,7 @@ void GCS_MAVLINK_Plane::send_pid_tuning()
     }
 #if HAL_QUADPLANE_ENABLED
     if (g.gcs_pid_mask & TUNING_BITS_ACCZ && plane.quadplane.in_vtol_mode()) {
-        pid_info = &plane.quadplane.pos_control->get_accel_U_pid().get_pid_info();
+        pid_info = &plane.quadplane.pos_control->D_get_accel_pid().get_pid_info();
         send_pid_info(pid_info, PID_TUNING_ACCZ, pid_info->actual);
     }
 #endif
@@ -1232,7 +1232,7 @@ int16_t GCS_MAVLINK_Plane::high_latency_target_altitude() const
     const QuadPlane &quadplane = plane.quadplane;
     //return units are m
     if (quadplane.show_vtol_view()) {
-        return (plane.control_mode != &plane.mode_qstabilize) ? (global_position_current.alt * 0.01 + quadplane.pos_control->get_pos_error_U_m()) : 0;
+        return (plane.control_mode != &plane.mode_qstabilize) ? (global_position_current.alt * 0.01 + quadplane.pos_control->get_pos_error_D_m()) : 0;
     }
 #endif
     return 0.01 * (global_position_current.alt + plane.calc_altitude_error_cm());

ArduPlane/VTOL_Assist.cpp
@@ -182,8 +182,8 @@ bool VTOL_Assist::check_VTOL_recovery(void)
                    controller may limit pitch after a strong
                    acceleration event
                 */
-                quadplane.pos_control->init_U_controller();
-                quadplane.pos_control->init_NE_controller();
+                quadplane.pos_control->D_init_controller();
+                quadplane.pos_control->NE_init_controller();
             }
         }
     }

ArduPlane/mode_qhover.cpp
@@ -7,8 +7,8 @@ bool ModeQHover::_enter()
 {
     // set vertical speed and acceleration limits
     // All limits must be positive
-    pos_control->set_max_speed_accel_U_m(quadplane.get_pilot_velocity_z_max_dn_m(), quadplane.pilot_speed_z_max_up_ms, quadplane.pilot_accel_z_mss);
-    pos_control->set_correction_speed_accel_U_m(quadplane.get_pilot_velocity_z_max_dn_m(), quadplane.pilot_speed_z_max_up_ms, quadplane.pilot_accel_z_mss);
+    pos_control->D_set_max_speed_accel_m(quadplane.get_pilot_velocity_z_max_dn_m(), quadplane.pilot_speed_z_max_up_ms, quadplane.pilot_accel_z_mss);
+    pos_control->D_set_correction_speed_accel_m(quadplane.get_pilot_velocity_z_max_dn_m(), quadplane.pilot_speed_z_max_up_ms, quadplane.pilot_accel_z_mss);
     quadplane.set_climb_rate_ms(0);
 
     quadplane.init_throttle_wait();
@@ -38,7 +38,7 @@ void ModeQHover::run()
         quadplane.set_desired_spool_state(AP_Motors::DesiredSpoolState::GROUND_IDLE);
         attitude_control->set_throttle_out(0, true, 0);
         quadplane.relax_attitude_control();
-        pos_control->relax_U_controller(0);
+        pos_control->D_relax_controller(0);
     } else {
         plane.quadplane.assign_tilt_to_fwd_thr();
         quadplane.hold_hover(quadplane.get_pilot_desired_climb_rate_cms());

ArduPlane/qautotune.cpp
@@ -42,10 +42,10 @@ void QAutoTune::init_z_limits()
 {
     // set vertical speed and acceleration limits
     // All limits must be positive
-    plane.quadplane.pos_control->set_max_speed_accel_U_m(plane.quadplane.get_pilot_velocity_z_max_dn_m(),
+    plane.quadplane.pos_control->D_set_max_speed_accel_m(plane.quadplane.get_pilot_velocity_z_max_dn_m(),
                                                        plane.quadplane.pilot_speed_z_max_up_ms,
                                                        plane.quadplane.pilot_accel_z_mss);
-    plane.quadplane.pos_control->set_correction_speed_accel_U_m(plane.quadplane.get_pilot_velocity_z_max_dn_m(),
+    plane.quadplane.pos_control->D_set_correction_speed_accel_m(plane.quadplane.get_pilot_velocity_z_max_dn_m(),
                                                               plane.quadplane.pilot_speed_z_max_up_ms,
                                                               plane.quadplane.pilot_accel_z_mss);
 }