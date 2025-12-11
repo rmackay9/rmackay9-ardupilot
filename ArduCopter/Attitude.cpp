ArduPilot/ardupilot

Copter: NEU to NED renaming - Conversion

ArduCopter/Attitude.cpp
@@ -123,7 +123,7 @@ void Copter::set_accel_throttle_I_from_pilot_throttle()
     // get last throttle input sent to attitude controller
     float pilot_throttle = constrain_float(attitude_control->get_throttle_in(), 0.0, 1.0);
     // shift difference between pilot's throttle and hover throttle into accelerometer I
-    pos_control->get_accel_D_pid().set_integrator((pilot_throttle-motors->get_throttle_hover()) * 1000.0);
+    pos_control->get_accel_D_pid().set_integrator(-(pilot_throttle - motors->get_throttle_hover()));
 }
 
 // It will return the PILOT_SPEED_DN value if non zero, otherwise if zero it returns the PILOT_SPEED_UP value.

rmackay9/rmackay9-ardupilot

ArduCopter/Attitude.cpp
@@ -123,7 +123,7 @@ void Copter::set_accel_throttle_I_from_pilot_throttle()
     // get last throttle input sent to attitude controller
     float pilot_throttle = constrain_float(attitude_control->get_throttle_in(), 0.0, 1.0);
     // shift difference between pilot's throttle and hover throttle into accelerometer I
-    pos_control->D_get_accel_pid().set_integrator((pilot_throttle-motors->get_throttle_hover()) * 1000.0);
+    pos_control->D_get_accel_pid().set_integrator(-(pilot_throttle - motors->get_throttle_hover()));
 }
 
 // It will return the PILOT_SPEED_DN value if non zero, otherwise if zero it returns the PILOT_SPEED_UP value.