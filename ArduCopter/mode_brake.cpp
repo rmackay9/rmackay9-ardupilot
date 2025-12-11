ArduPilot/ardupilot

Copter: NEU to NED renaming - No compiler change

ArduCopter/mode_brake.cpp
@@ -10,19 +10,19 @@
 bool ModeBrake::init(bool ignore_checks)
 {
     // initialise pos controller speed and acceleration
-    pos_control->set_max_speed_accel_NE_m(pos_control->get_vel_estimate_NEU_ms().xy().length(), BRAKE_MODE_DECEL_RATE_MSS);
-    pos_control->set_correction_speed_accel_NE_m(pos_control->get_vel_estimate_NEU_ms().xy().length(), BRAKE_MODE_DECEL_RATE_MSS);
+    pos_control->set_max_speed_accel_NE_m(pos_control->get_vel_estimate_NED_ms().xy().length(), BRAKE_MODE_DECEL_RATE_MSS);
+    pos_control->set_correction_speed_accel_NE_m(pos_control->get_vel_estimate_NED_ms().xy().length(), BRAKE_MODE_DECEL_RATE_MSS);
 
     // initialise position controller
     pos_control->init_NE_controller();
 
     // set vertical speed and acceleration limits
-    pos_control->set_max_speed_accel_U_m(BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_DECEL_RATE_MSS);
-    pos_control->set_correction_speed_accel_U_m(BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_DECEL_RATE_MSS);
+    pos_control->set_max_speed_accel_D_m(BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_DECEL_RATE_MSS);
+    pos_control->set_correction_speed_accel_D_m(BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_DECEL_RATE_MSS);
 
     // initialise the vertical position controller
-    if (!pos_control->is_active_U()) {
-        pos_control->init_U_controller();
+    if (!pos_control->is_active_D()) {
+        pos_control->init_D_controller();
     }
 
     _timeout_ms = 0;
@@ -37,7 +37,7 @@ void ModeBrake::run()
     // if not armed set throttle to zero and exit immediately
     if (is_disarmed_or_landed()) {
         make_safe_ground_handling();
-        pos_control->relax_U_controller(0.0f);
+        pos_control->relax_D_controller(0.0f);
         return;
     }
 
@@ -58,8 +58,8 @@ void ModeBrake::run()
     // call attitude controller
     attitude_control->input_thrust_vector_rate_heading_rads(pos_control->get_thrust_vector(), 0.0f);
 
-    pos_control->set_pos_target_U_from_climb_rate_ms(0.0f);
-    pos_control->update_U_controller();
+    pos_control->set_pos_target_D_from_climb_rate_ms(0.0f);
+    pos_control->update_D_controller();
 
     // MAV_CMD_SOLO_BTN_PAUSE_CLICK (Solo only) is used to set the timeout.
     if (_timeout_ms != 0 && millis()-_timeout_start >= _timeout_ms) {

ArduCopter/mode_loiter.cpp
@@ -22,13 +22,13 @@ bool ModeLoiter::init(bool ignore_checks)
     loiter_nav->init_target();
 
     // initialise the vertical position controller
-    if (!pos_control->is_active_U()) {
-        pos_control->init_U_controller();
+    if (!pos_control->is_active_D()) {
+        pos_control->init_D_controller();
     }
 
     // set vertical speed and acceleration limits
-    pos_control->set_max_speed_accel_U_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
-    pos_control->set_correction_speed_accel_U_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
+    pos_control->set_max_speed_accel_D_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_D_mss());
+    pos_control->set_correction_speed_accel_D_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_D_mss());
 
 #if AC_PRECLAND_ENABLED
     _precision_loiter_active = false;
@@ -62,10 +62,10 @@ void ModeLoiter::precision_loiter_xy()
     Vector2p target_pos_ne_m;
     Vector2f target_vel_ne_ms;
     if (!copter.precland.get_target_position_m(target_pos_ne_m)) {
-        target_pos_ne_m = pos_control->get_pos_estimate_NEU_m().xy();
+        target_pos_ne_m = pos_control->get_pos_estimate_NED_m().xy();
     }
     // get the velocity of the target
-    copter.precland.get_target_velocity_ms(pos_control->get_vel_estimate_NEU_ms().xy(), target_vel_ne_ms);
+    copter.precland.get_target_velocity_ms(pos_control->get_vel_estimate_NED_ms().xy(), target_vel_ne_ms);
 
     Vector2f zero;
     // target vel will remain zero if landing target is stationary
@@ -84,7 +84,7 @@ void ModeLoiter::run()
     float target_climb_rate_ms = 0.0f;
 
     // set vertical speed and acceleration limits
-    pos_control->set_max_speed_accel_U_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
+    pos_control->set_max_speed_accel_D_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_D_mss());
 
     // apply SIMPLE mode transform to pilot inputs
     update_simple_mode();
@@ -108,7 +108,7 @@ void ModeLoiter::run()
     }
 
     // Loiter State Machine Determination
-    AltHoldModeState loiter_state = get_alt_hold_state_U_ms(target_climb_rate_ms);
+    AltHoldModeState loiter_state = get_alt_hold_state_D_ms(target_climb_rate_ms);
 
     // Loiter State Machine
     switch (loiter_state) {
@@ -116,7 +116,7 @@ void ModeLoiter::run()
     case AltHoldModeState::MotorStopped:
         attitude_control->reset_rate_controller_I_terms();
         attitude_control->reset_yaw_target_and_rate();
-        pos_control->relax_U_controller(0.0f);   // forces throttle output to decay to zero
+        pos_control->relax_D_controller(0.0f);   // forces throttle output to decay to zero
         loiter_nav->init_target();
         break;
 
@@ -127,7 +127,7 @@ void ModeLoiter::run()
     case AltHoldModeState::Landed_Pre_Takeoff:
         attitude_control->reset_rate_controller_I_terms_smoothly();
         loiter_nav->init_target();
-        pos_control->relax_U_controller(0.0f);   // forces throttle output to decay to zero
+        pos_control->relax_D_controller(0.0f);   // forces throttle output to decay to zero
         break;
 
     case AltHoldModeState::Takeoff:
@@ -180,14 +180,14 @@ void ModeLoiter::run()
 #endif
 
         // Send the commanded climb rate to the position controller
-        pos_control->set_pos_target_U_from_climb_rate_ms(target_climb_rate_ms);
+        pos_control->set_pos_target_D_from_climb_rate_ms(target_climb_rate_ms);
         break;
     }
 
     // call attitude controller
     attitude_control->input_thrust_vector_rate_heading_rads(loiter_nav->get_thrust_vector(), target_yaw_rate_rads, false);
     // run the vertical position controller and set output throttle
-    pos_control->update_U_controller();
+    pos_control->update_D_controller();
 }
 
 float ModeLoiter::wp_distance_m() const

ArduCopter/mode_rtl.cpp
@@ -175,7 +175,7 @@ void ModeRTL::climb_return_run()
 
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
-    pos_control->update_U_controller();
+    pos_control->update_D_controller();
 
     // call attitude controller with auto yaw
     attitude_control->input_thrust_vector_heading(pos_control->get_thrust_vector(), auto_yaw.get_heading());
@@ -217,7 +217,7 @@ void ModeRTL::loiterathome_run()
 
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
-    pos_control->update_U_controller();
+    pos_control->update_D_controller();
 
     // call attitude controller with auto yaw
     attitude_control->input_thrust_vector_heading(pos_control->get_thrust_vector(), auto_yaw.get_heading());
@@ -244,7 +244,7 @@ void ModeRTL::descent_start()
     _state_complete = false;
 
     // initialise altitude target to stopping point
-    pos_control->init_U_controller_stopping_point();
+    pos_control->init_D_controller_stopping_point();
 
     // initialise yaw
     auto_yaw.set_mode(AutoYaw::Mode::HOLD);
@@ -304,7 +304,7 @@ void ModeRTL::descent_run()
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
     pos_control->set_alt_target_with_slew_m(rtl_path.descent_target.alt * 0.01);
-    pos_control->update_U_controller();
+    pos_control->update_D_controller();
 
     // roll & pitch from waypoint controller, yaw rate from pilot
     attitude_control->input_thrust_vector_heading(pos_control->get_thrust_vector(), auto_yaw.get_heading());
@@ -329,8 +329,8 @@ void ModeRTL::land_start()
     }
 
     // initialise the vertical position controller
-    if (!pos_control->is_active_U()) {
-        pos_control->init_U_controller();
+    if (!pos_control->is_active_D()) {
+        pos_control->init_D_controller();
     }
 
     // initialise yaw
@@ -407,10 +407,10 @@ void ModeRTL::compute_return_target()
 #endif
 
     // get position controller Z-axis offset in cm above EKF origin
-    float pos_offset_u_m = pos_control->get_pos_offset_U_m();
+    float pos_offset_d_m = pos_control->get_pos_offset_D_m();
 
     // curr_alt_m is current altitude above home or above terrain depending upon use_terrain
-    float curr_alt_m = copter.current_loc.alt * 0.01 - pos_offset_u_m;
+    float curr_alt_m = copter.current_loc.alt * 0.01 - pos_offset_d_m;
 
     // determine altitude type of return journey (alt-above-home, alt-above-terrain using range finder or alt-above-terrain using terrain database)
     ReturnTargetAltType alt_type = ReturnTargetAltType::RELATIVE;
@@ -435,7 +435,7 @@ void ModeRTL::compute_return_target()
     if (alt_type == ReturnTargetAltType::RANGEFINDER) {
         if (copter.get_rangefinder_height_interpolated_m(curr_alt_m)) {
             // subtract position controller offset
-            curr_alt_m -= pos_offset_u_m;
+            curr_alt_m -= pos_offset_d_m;
             // set return_target.alt
             rtl_path.return_target.set_alt_m(MAX(curr_alt_m + MAX(0.0, g.rtl_climb_min_cm * 0.01), MAX(g.rtl_altitude_cm * 0.01, RTL_ALT_MIN_M)), Location::AltFrame::ABOVE_TERRAIN);
         } else {
@@ -454,7 +454,7 @@ void ModeRTL::compute_return_target()
         float curr_terr_alt_m;
         if (copter.current_loc.get_alt_m(Location::AltFrame::ABOVE_TERRAIN, curr_terr_alt_m) &&
             rtl_path.return_target.change_alt_frame(Location::AltFrame::ABOVE_TERRAIN)) {
-            curr_alt_m = curr_terr_alt_m - pos_offset_u_m;
+            curr_alt_m = curr_terr_alt_m - pos_offset_d_m;
         } else {
             // fallback to relative alt and warn user
             alt_type = ReturnTargetAltType::RELATIVE;

ArduCopter/standby.cpp
@@ -19,5 +19,5 @@ void Copter::standby_update()
 
     attitude_control->reset_rate_controller_I_terms();
     attitude_control->reset_yaw_target_and_rate();
-    pos_control->standby_NEU_reset();
+    pos_control->standby_NED_reset();
 }

ArduCopter/tuning.cpp
@@ -92,23 +92,23 @@ void Copter::tuning(const RC_Channel *tuning_ch, int8_t tuning_param, float tuni
 
     // Altitude and throttle tuning
     case TUNING_ALTITUDE_HOLD_KP:
-        pos_control->get_pos_U_p().set_kP(tuning_value);
+        pos_control->get_pos_D_p().set_kP(tuning_value);
         break;
 
     case TUNING_THROTTLE_RATE_KP:
-        pos_control->get_vel_U_pid().set_kP(tuning_value);
+        pos_control->get_vel_D_pid().set_kP(tuning_value);
         break;
 
     case TUNING_ACCEL_Z_KP:
-        pos_control->get_accel_U_pid().set_kP(tuning_value);
+        pos_control->get_accel_D_pid().set_kP(tuning_value);
         break;
 
     case TUNING_ACCEL_Z_KI:
-        pos_control->get_accel_U_pid().set_kI(tuning_value);
+        pos_control->get_accel_D_pid().set_kI(tuning_value);
         break;
 
     case TUNING_ACCEL_Z_KD:
-        pos_control->get_accel_U_pid().set_kD(tuning_value);
+        pos_control->get_accel_D_pid().set_kD(tuning_value);
         break;
 
     // Loiter and navigation tuning

rmackay9/rmackay9-ardupilot

ArduCopter/mode_brake.cpp
@@ -10,19 +10,19 @@
 bool ModeBrake::init(bool ignore_checks)
 {
     // initialise pos controller speed and acceleration
-    pos_control->set_max_speed_accel_NE_m(pos_control->get_vel_estimate_NEU_ms().xy().length(), BRAKE_MODE_DECEL_RATE_MSS);
-    pos_control->set_correction_speed_accel_NE_m(pos_control->get_vel_estimate_NEU_ms().xy().length(), BRAKE_MODE_DECEL_RATE_MSS);
+    pos_control->NE_set_max_speed_accel_m(pos_control->get_vel_estimate_NED_ms().xy().length(), BRAKE_MODE_DECEL_RATE_MSS);
+    pos_control->NE_set_correction_speed_accel_m(pos_control->get_vel_estimate_NED_ms().xy().length(), BRAKE_MODE_DECEL_RATE_MSS);
 
     // initialise position controller
-    pos_control->init_NE_controller();
+    pos_control->NE_init_controller();
 
     // set vertical speed and acceleration limits
-    pos_control->set_max_speed_accel_U_m(BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_DECEL_RATE_MSS);
-    pos_control->set_correction_speed_accel_U_m(BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_DECEL_RATE_MSS);
+    pos_control->D_set_max_speed_accel_m(BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_DECEL_RATE_MSS);
+    pos_control->D_set_correction_speed_accel_m(BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_SPEED_Z_MS, BRAKE_MODE_DECEL_RATE_MSS);
 
     // initialise the vertical position controller
-    if (!pos_control->is_active_U()) {
-        pos_control->init_U_controller();
+    if (!pos_control->D_is_active()) {
+        pos_control->D_init_controller();
     }
 
     _timeout_ms = 0;
@@ -37,7 +37,7 @@ void ModeBrake::run()
     // if not armed set throttle to zero and exit immediately
     if (is_disarmed_or_landed()) {
         make_safe_ground_handling();
-        pos_control->relax_U_controller(0.0f);
+        pos_control->D_relax_controller(0.0f);
         return;
     }
 
@@ -46,20 +46,20 @@ void ModeBrake::run()
 
     // relax stop target if we might be landed
     if (copter.ap.land_complete_maybe) {
-        pos_control->soften_for_landing_NE();
+        pos_control->NE_soften_for_landing();
     }
 
     // use position controller to stop
     Vector2f vel;
     Vector2f accel;
     pos_control->input_vel_accel_NE_m(vel, accel);
-    pos_control->update_NE_controller();
+    pos_control->NE_update_controller();
 
     // call attitude controller
     attitude_control->input_thrust_vector_rate_heading_rads(pos_control->get_thrust_vector(), 0.0f);
 
-    pos_control->set_pos_target_U_from_climb_rate_ms(0.0f);
-    pos_control->update_U_controller();
+    pos_control->D_set_pos_target_from_climb_rate_ms(0.0f);
+    pos_control->D_update_controller();
 
     // MAV_CMD_SOLO_BTN_PAUSE_CLICK (Solo only) is used to set the timeout.
     if (_timeout_ms != 0 && millis()-_timeout_start >= _timeout_ms) {

ArduCopter/mode_loiter.cpp
@@ -22,13 +22,13 @@ bool ModeLoiter::init(bool ignore_checks)
     loiter_nav->init_target();
 
     // initialise the vertical position controller
-    if (!pos_control->is_active_U()) {
-        pos_control->init_U_controller();
+    if (!pos_control->D_is_active()) {
+        pos_control->D_init_controller();
     }
 
     // set vertical speed and acceleration limits
-    pos_control->set_max_speed_accel_U_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
-    pos_control->set_correction_speed_accel_U_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
+    pos_control->D_set_max_speed_accel_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_D_mss());
+    pos_control->D_set_correction_speed_accel_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_D_mss());
 
 #if AC_PRECLAND_ENABLED
     _precision_loiter_active = false;
@@ -62,16 +62,16 @@ void ModeLoiter::precision_loiter_xy()
     Vector2p target_pos_ne_m;
     Vector2f target_vel_ne_ms;
     if (!copter.precland.get_target_position_m(target_pos_ne_m)) {
-        target_pos_ne_m = pos_control->get_pos_estimate_NEU_m().xy();
+        target_pos_ne_m = pos_control->get_pos_estimate_NED_m().xy();
     }
     // get the velocity of the target
-    copter.precland.get_target_velocity_ms(pos_control->get_vel_estimate_NEU_ms().xy(), target_vel_ne_ms);
+    copter.precland.get_target_velocity_ms(pos_control->get_vel_estimate_NED_ms().xy(), target_vel_ne_ms);
 
     Vector2f zero;
     // target vel will remain zero if landing target is stationary
     pos_control->input_pos_vel_accel_NE_m(target_pos_ne_m, target_vel_ne_ms, zero);
     // run pos controller
-    pos_control->update_NE_controller();
+    pos_control->NE_update_controller();
 }
 #endif
 
@@ -84,7 +84,7 @@ void ModeLoiter::run()
     float target_climb_rate_ms = 0.0f;
 
     // set vertical speed and acceleration limits
-    pos_control->set_max_speed_accel_U_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_U_mss());
+    pos_control->D_set_max_speed_accel_m(get_pilot_speed_dn_ms(), get_pilot_speed_up_ms(), get_pilot_accel_D_mss());
 
     // apply SIMPLE mode transform to pilot inputs
     update_simple_mode();
@@ -108,7 +108,7 @@ void ModeLoiter::run()
     }
 
     // Loiter State Machine Determination
-    AltHoldModeState loiter_state = get_alt_hold_state_U_ms(target_climb_rate_ms);
+    AltHoldModeState loiter_state = get_alt_hold_state_D_ms(target_climb_rate_ms);
 
     // Loiter State Machine
     switch (loiter_state) {
@@ -116,7 +116,7 @@ void ModeLoiter::run()
     case AltHoldModeState::MotorStopped:
         attitude_control->reset_rate_controller_I_terms();
         attitude_control->reset_yaw_target_and_rate();
-        pos_control->relax_U_controller(0.0f);   // forces throttle output to decay to zero
+        pos_control->D_relax_controller(0.0f);   // forces throttle output to decay to zero
         loiter_nav->init_target();
         break;
 
@@ -127,7 +127,7 @@ void ModeLoiter::run()
     case AltHoldModeState::Landed_Pre_Takeoff:
         attitude_control->reset_rate_controller_I_terms_smoothly();
         loiter_nav->init_target();
-        pos_control->relax_U_controller(0.0f);   // forces throttle output to decay to zero
+        pos_control->D_relax_controller(0.0f);   // forces throttle output to decay to zero
         break;
 
     case AltHoldModeState::Takeoff:
@@ -180,14 +180,14 @@ void ModeLoiter::run()
 #endif
 
         // Send the commanded climb rate to the position controller
-        pos_control->set_pos_target_U_from_climb_rate_ms(target_climb_rate_ms);
+        pos_control->D_set_pos_target_from_climb_rate_ms(target_climb_rate_ms);
         break;
     }
 
     // call attitude controller
     attitude_control->input_thrust_vector_rate_heading_rads(loiter_nav->get_thrust_vector(), target_yaw_rate_rads, false);
     // run the vertical position controller and set output throttle
-    pos_control->update_U_controller();
+    pos_control->D_update_controller();
 }
 
 float ModeLoiter::wp_distance_m() const

ArduCopter/mode_rtl.cpp
@@ -175,7 +175,7 @@ void ModeRTL::climb_return_run()
 
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
-    pos_control->update_U_controller();
+    pos_control->D_update_controller();
 
     // call attitude controller with auto yaw
     attitude_control->input_thrust_vector_heading(pos_control->get_thrust_vector(), auto_yaw.get_heading());
@@ -217,7 +217,7 @@ void ModeRTL::loiterathome_run()
 
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
-    pos_control->update_U_controller();
+    pos_control->D_update_controller();
 
     // call attitude controller with auto yaw
     attitude_control->input_thrust_vector_heading(pos_control->get_thrust_vector(), auto_yaw.get_heading());
@@ -244,7 +244,7 @@ void ModeRTL::descent_start()
     _state_complete = false;
 
     // initialise altitude target to stopping point
-    pos_control->init_U_controller_stopping_point();
+    pos_control->D_init_controller_stopping_point();
 
     // initialise yaw
     auto_yaw.set_mode(AutoYaw::Mode::HOLD);
@@ -299,12 +299,12 @@ void ModeRTL::descent_run()
 
     Vector2f accel;
     pos_control->input_vel_accel_NE_m(vel_correction_ms, accel);
-    pos_control->update_NE_controller();
+    pos_control->NE_update_controller();
 
     // WP_Nav has set the vertical position control targets
     // run the vertical position controller and set output throttle
-    pos_control->set_alt_target_with_slew_m(rtl_path.descent_target.alt * 0.01);
-    pos_control->update_U_controller();
+    pos_control->D_set_alt_target_with_slew_m(rtl_path.descent_target.alt * 0.01);
+    pos_control->D_update_controller();
 
     // roll & pitch from waypoint controller, yaw rate from pilot
     attitude_control->input_thrust_vector_heading(pos_control->get_thrust_vector(), auto_yaw.get_heading());
@@ -320,17 +320,17 @@ void ModeRTL::land_start()
     _state_complete = false;
 
     // set horizontal speed and acceleration limits
-    pos_control->set_max_speed_accel_NE_m(wp_nav->get_default_speed_NE_ms(), wp_nav->get_wp_acceleration_mss());
-    pos_control->set_correction_speed_accel_NE_m(wp_nav->get_default_speed_NE_ms(), wp_nav->get_wp_acceleration_mss());
+    pos_control->NE_set_max_speed_accel_m(wp_nav->get_default_speed_NE_ms(), wp_nav->get_wp_acceleration_mss());
+    pos_control->NE_set_correction_speed_accel_m(wp_nav->get_default_speed_NE_ms(), wp_nav->get_wp_acceleration_mss());
 
     // initialise loiter target destination
-    if (!pos_control->is_active_NE()) {
-        pos_control->init_NE_controller();
+    if (!pos_control->NE_is_active()) {
+        pos_control->NE_init_controller();
     }
 
     // initialise the vertical position controller
-    if (!pos_control->is_active_U()) {
-        pos_control->init_U_controller();
+    if (!pos_control->D_is_active()) {
+        pos_control->D_init_controller();
     }
 
     // initialise yaw
@@ -407,10 +407,10 @@ void ModeRTL::compute_return_target()
 #endif
 
     // get position controller Z-axis offset in cm above EKF origin
-    float pos_offset_u_m = pos_control->get_pos_offset_U_m();
+    float pos_offset_d_m = pos_control->get_pos_offset_U_m();
 
     // curr_alt_m is current altitude above home or above terrain depending upon use_terrain
-    float curr_alt_m = copter.current_loc.alt * 0.01 - pos_offset_u_m;
+    float curr_alt_m = copter.current_loc.alt * 0.01 - pos_offset_d_m;
 
     // determine altitude type of return journey (alt-above-home, alt-above-terrain using range finder or alt-above-terrain using terrain database)
     ReturnTargetAltType alt_type = ReturnTargetAltType::RELATIVE;
@@ -435,7 +435,7 @@ void ModeRTL::compute_return_target()
     if (alt_type == ReturnTargetAltType::RANGEFINDER) {
         if (copter.get_rangefinder_height_interpolated_m(curr_alt_m)) {
             // subtract position controller offset
-            curr_alt_m -= pos_offset_u_m;
+            curr_alt_m -= pos_offset_d_m;
             // set return_target.alt
             rtl_path.return_target.set_alt_m(MAX(curr_alt_m + MAX(0.0, g.rtl_climb_min_cm * 0.01), MAX(g.rtl_altitude_cm * 0.01, RTL_ALT_MIN_M)), Location::AltFrame::ABOVE_TERRAIN);
         } else {
@@ -454,7 +454,7 @@ void ModeRTL::compute_return_target()
         float curr_terr_alt_m;
         if (copter.current_loc.get_alt_m(Location::AltFrame::ABOVE_TERRAIN, curr_terr_alt_m) &&
             rtl_path.return_target.change_alt_frame(Location::AltFrame::ABOVE_TERRAIN)) {
-            curr_alt_m = curr_terr_alt_m - pos_offset_u_m;
+            curr_alt_m = curr_terr_alt_m - pos_offset_d_m;
         } else {
             // fallback to relative alt and warn user
             alt_type = ReturnTargetAltType::RELATIVE;

ArduCopter/standby.cpp
@@ -19,5 +19,5 @@ void Copter::standby_update()
 
     attitude_control->reset_rate_controller_I_terms();
     attitude_control->reset_yaw_target_and_rate();
-    pos_control->standby_NEU_reset();
+    pos_control->NED_standby_reset();
 }

ArduCopter/tuning.cpp
@@ -92,36 +92,36 @@ void Copter::tuning(const RC_Channel *tuning_ch, int8_t tuning_param, float tuni
 
     // Altitude and throttle tuning
     case TUNING_ALTITUDE_HOLD_KP:
-        pos_control->get_pos_U_p().set_kP(tuning_value);
+        pos_control->D_get_pos_p().set_kP(tuning_value);
         break;
 
     case TUNING_THROTTLE_RATE_KP:
-        pos_control->get_vel_U_pid().set_kP(tuning_value);
+        pos_control->D_get_vel_pid().set_kP(tuning_value);
         break;
 
     case TUNING_ACCEL_Z_KP:
-        pos_control->get_accel_U_pid().set_kP(tuning_value);
+        pos_control->D_get_accel_pid().set_kP(tuning_value);
         break;
 
     case TUNING_ACCEL_Z_KI:
-        pos_control->get_accel_U_pid().set_kI(tuning_value);
+        pos_control->D_get_accel_pid().set_kI(tuning_value);
         break;
 
     case TUNING_ACCEL_Z_KD:
-        pos_control->get_accel_U_pid().set_kD(tuning_value);
+        pos_control->D_get_accel_pid().set_kD(tuning_value);
         break;
 
     // Loiter and navigation tuning
     case TUNING_LOITER_POSITION_KP:
-        pos_control->get_pos_NE_p().set_kP(tuning_value);
+        pos_control->NE_get_pos_p().set_kP(tuning_value);
         break;
 
     case TUNING_VEL_XY_KP:
-        pos_control->get_vel_NE_pid().set_kP(tuning_value);
+        pos_control->NE_get_vel_pid().set_kP(tuning_value);
         break;
 
     case TUNING_VEL_XY_KI:
-        pos_control->get_vel_NE_pid().set_kI(tuning_value);
+        pos_control->NE_get_vel_pid().set_kI(tuning_value);
         break;
 
     case TUNING_WP_SPEED: