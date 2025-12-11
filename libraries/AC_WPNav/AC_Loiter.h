ArduPilot/ardupilot

AC_WPNav: NEU to NED renaming - No compiler change

libraries/AC_WPNav/AC_Loiter.h
@@ -16,9 +16,9 @@ public:
     AC_Loiter(const AP_AHRS_View& ahrs, AC_PosControl& pos_control, const AC_AttitudeControl& attitude_control);
 
     // Sets the initial loiter target position in meters from the EKF origin.
-    // - position_neu_m: horizontal position in the NE frame, in meters.
+    // - position_ned_m: horizontal position in the NE frame, in meters.
     // - Initializes internal control state including acceleration targets and feed-forward planning.
-    void init_target_m(const Vector2p& position_neu_m);
+    void init_target_m(const Vector2p& position_ned_m);
 
     // Initializes the loiter controller using the current position and velocity.
     // Updates feed-forward velocity, predicted acceleration, and resets control state.

rmackay9/rmackay9-ardupilot

libraries/AC_WPNav/AC_Loiter.h
@@ -16,9 +16,9 @@ public:
     AC_Loiter(const AP_AHRS_View& ahrs, AC_PosControl& pos_control, const AC_AttitudeControl& attitude_control);
 
     // Sets the initial loiter target position in meters from the EKF origin.
-    // - position_neu_m: horizontal position in the NE frame, in meters.
+    // - position_ne_m: horizontal position in the NE frame, in meters.
     // - Initializes internal control state including acceleration targets and feed-forward planning.
-    void init_target_m(const Vector2p& position_neu_m);
+    void init_target_m(const Vector2p& position_ne_m);
 
     // Initializes the loiter controller using the current position and velocity.
     // Updates feed-forward velocity, predicted acceleration, and resets control state.